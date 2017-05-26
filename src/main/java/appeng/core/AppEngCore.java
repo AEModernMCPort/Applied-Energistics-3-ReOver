fddc6788727fbafc7f1c56d8751236e05bd4aded
elix-x <Elix-x@users.noreply.github.com>
2017-05-26 15:09:04 +0200

    Removed old mess from core module

--- src/main/java/appeng/core/AppEngCore.java
+++ src/main/java/appeng/core/AppEngCore.java
@@ -7,35 +7,22 @@ import appeng.api.module.Module.ModuleEventHandler;
 import appeng.core.api.ICore;
 import appeng.core.api.material.Material;
 import appeng.core.definitions.*;
-import appeng.core.hooks.TickHandler;
 import appeng.core.lib.bootstrap.FeatureFactory;
-import appeng.core.lib.sync.GuiBridge;
-import appeng.core.lib.sync.network.NetworkHandler;
-import appeng.core.lib.worlddata.WorldData;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.*;
 import net.minecraftforge.fml.common.event.FMLInterModComms.IMCEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.FMLControlledNamespacedRegistry;
 import net.minecraftforge.fml.common.registry.RegistryBuilder;
 
-import javax.annotation.Nonnull;
-
-/*
- * TODO 1.10.2-MODUSEP - Dat giant mess though. Move all stuff that belongs to specific modules into these specific modules. Yes, you can boom the API.
- */
 @Module(value = ICore.NAME, dependencies = "hard-before:module-*")
 public class AppEngCore implements ICore {
 
 	@Module.Instance(NAME)
 	public static final AppEngCore INSTANCE = null;
-
-	private final Registration registration;
 
 	private FMLControlledNamespacedRegistry<Material> materialRegistry;
 
@@ -48,7 +35,7 @@ public class AppEngCore implements ICore {
 	private CoreEntityDefinitions entityDefinitions;
 
 	public AppEngCore(){
-		this.registration = new Registration();
+
 	}
 
 	@Override
@@ -71,11 +58,6 @@ public class AppEngCore implements ICore {
 		return null;
 	}
 
-	@Nonnull
-	public final Registration getRegistration(){
-		return this.registration;
-	}
-
 	public FMLControlledNamespacedRegistry<Material> getMaterialRegistry(){
 		return materialRegistry;
 	}
@@ -91,63 +73,41 @@ public class AppEngCore implements ICore {
 		this.tileDefinitions = new CoreTileDefinitions(registry);
 		this.entityDefinitions = new CoreEntityDefinitions(registry);
 		registry.preInit(event);
-
-		CreativeTab.init();
-
-		this.registration.preInitialize(event);
-	}
-
-	private void startService(final String serviceName, final Thread thread){
-		thread.setName(serviceName);
-		thread.setPriority(Thread.MIN_PRIORITY);
-
-		AELog.info("Starting " + serviceName);
-		thread.start();
 	}
 
 	@ModuleEventHandler
 	public void init(FMLInitializationEvent event){
-		this.registration.initialize(event);
-
 		registry.init(event);
 	}
 
 	@ModuleEventHandler
 	public void postInit(FMLPostInitializationEvent event){
-		this.registration.postInit(event);
-
 		registry.postInit(event);
-
-		NetworkRegistry.INSTANCE.registerGuiHandler(this, GuiBridge.GUI_Handler);
-		NetworkHandler.instance = new NetworkHandler("AE2");
 	}
 
 	@ModuleEventHandler
 	public void handleIMCEvent(IMCEvent event){
-		final IMCHandler imcHandler = new IMCHandler();
 
-		imcHandler.handleIMCEvent(event);
 	}
 
 	@ModuleEventHandler
 	public void serverAboutToStart(FMLServerAboutToStartEvent event){
-		WorldData.onServerAboutToStart(event.getServer());
+
 	}
 
 	@ModuleEventHandler
 	public void serverStarting(FMLServerStartingEvent event){
-		event.registerServerCommand(new AECommand(event.getServer()));
+
 	}
 
 	@ModuleEventHandler
 	public void serverStopping(FMLServerStoppingEvent event){
-		WorldData.instance().onServerStopping();
+
 	}
 
 	@ModuleEventHandler
 	public void serverStopped(FMLServerStoppedEvent event){
-		WorldData.instance().onServerStoppped();
-		TickHandler.INSTANCE.shutdown();
+
 	}
 
 }

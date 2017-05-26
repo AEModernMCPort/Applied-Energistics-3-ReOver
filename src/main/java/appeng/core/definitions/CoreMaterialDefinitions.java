8d90f3317f604cb85b9de8b9c501cc9edc510b64
elix-x <Elix-x@users.noreply.github.com>
2017-05-26 19:17:23 +0200

    I either register default invalid material, or NPEs will haunt me for ever

--- src/main/java/appeng/core/definitions/CoreMaterialDefinitions.java
+++ src/main/java/appeng/core/definitions/CoreMaterialDefinitions.java
@@ -1,10 +1,12 @@
 package appeng.core.definitions;
 
 import appeng.api.definitions.IMaterialDefinition;
+import appeng.core.AppEng;
 import appeng.core.api.definitions.ICoreMaterialDefinitions;
 import appeng.core.api.material.Material;
 import appeng.core.lib.bootstrap.FeatureFactory;
 import appeng.core.lib.definitions.Definitions;
+import net.minecraft.util.ResourceLocation;
 
 public class CoreMaterialDefinitions extends Definitions<Material, IMaterialDefinition<Material>>
 		implements ICoreMaterialDefinitions {
@@ -13,6 +15,8 @@ public class CoreMaterialDefinitions extends Definitions<Material, IMaterialDefi
 	private static final String MATERIALSMODELSVARIANT = "inventory";
 
 	public CoreMaterialDefinitions(FeatureFactory registry){
+		registry.material(new ResourceLocation(AppEng.MODID, "invalid"), new Material()).build();
+
 		init();
 	}
 

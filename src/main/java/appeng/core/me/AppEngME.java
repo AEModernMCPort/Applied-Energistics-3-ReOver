b17ef5ee49405c5313e92d07b6d52be3b04cd19d
elix-x <Elix-x@users.noreply.github.com>
2017-05-26 15:19:41 +0200

    Fixed ME module

--- src/main/java/appeng/core/me/AppEngME.java
+++ src/main/java/appeng/core/me/AppEngME.java
@@ -5,41 +5,21 @@ import appeng.api.definitions.IDefinitions;
 import appeng.api.module.Module;
 import appeng.api.module.Module.ModuleEventHandler;
 import appeng.api.module.ModuleIMCMessageEvent;
-import appeng.core.AppEng;
 import appeng.core.api.material.Material;
-import appeng.core.lib.features.AEFeature;
+import appeng.core.lib.bootstrap.FeatureFactory;
 import appeng.core.me.api.IME;
-import appeng.core.me.api.part.PartRegistryEntry;
-import appeng.core.me.api.parts.IPart;
 import appeng.core.me.definitions.*;
-import appeng.core.me.item.ItemCard;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.event.*;
 import net.minecraftforge.fml.common.registry.EntityEntry;
-import net.minecraftforge.fml.common.registry.FMLControlledNamespacedRegistry;
-
-import java.io.File;
-import java.util.Map;
 
 @Module(IME.NAME)
 public class AppEngME implements IME {
 
 	@Module.Instance(NAME)
 	public static final AppEngME INSTANCE = null;
-
-	private static final ResourceLocation CLASS2PARTMAP = new ResourceLocation(AppEng.MODID, "class-partrege_map");
-
-	public final ItemCard.EnumCardType CAPACITY = ItemCard.EnumCardType.addCardType("CAPACITY");
-	public final ItemCard.EnumCardType REDSTONE = ItemCard.EnumCardType.addCardType("REDSTONE");
-
-	public final ItemCard.EnumCardType FUZZY = ItemCard.EnumCardType.addCardType("FUZZY");
-	public final ItemCard.EnumCardType INVERTER = ItemCard.EnumCardType.addCardType("INVERTER");
-	public final ItemCard.EnumCardType ACCELERATION = ItemCard.EnumCardType.addCardType("ACCELERATION");
-
-	private FMLControlledNamespacedRegistry<PartRegistryEntry> partRegistry;
 
 	private FeatureFactory registry;
 
@@ -69,14 +49,6 @@ public class AppEngME implements IME {
 		return null;
 	}
 
-	private Map<Class<?>, ResourceLocation> getClass2PartMap(Map<ResourceLocation, ?> slaveset){
-		return (Map<Class<?>, ResourceLocation>) slaveset.get(CLASS2PARTMAP);
-	}
-
-	public ResourceLocation getRegistryName(Class<? extends IPart> clas){
-		return (ResourceLocation) partRegistry.getSlaveMap(CLASS2PARTMAP, Map.class).get(clas);
-	}
-
 	@ModuleEventHandler
 	public void preInit(FMLPreInitializationEvent event){
 		registry = new FeatureFactory();
@@ -86,11 +58,6 @@ public class AppEngME implements IME {
 		this.tileDefinitions = new METileDefinitions(registry);
 		this.entityDefinitions = new MEEntityDefinitions(registry);
 		registry.preInit(event);
-
-		FacadeConfig.instance = new FacadeConfig(new File(AppEng.instance().getConfigDirectory(), "Facades.cfg"));
-		if(AEConfig.instance.isFeatureEnabled(AEFeature.Facades)){
-			CreativeTabFacade.init();
-		}
 	}
 
 	@ModuleEventHandler

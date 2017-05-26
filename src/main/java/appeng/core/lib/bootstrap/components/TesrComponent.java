37a3157761bb6c213271e8f88f41511afbca48bd
elix-x <Elix-x@users.noreply.github.com>
2017-05-26 15:24:36 +0200

    Tesr component works on everything

--- src/main/java/appeng/core/lib/bootstrap/components/TesrComponent.java
+++ src/main/java/appeng/core/lib/bootstrap/components/TesrComponent.java
@@ -1,7 +1,7 @@
 package appeng.core.lib.bootstrap.components;
 
-import appeng.core.lib.tile.AEBaseTile;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.relauncher.Side;
  *
  * @param <T>
  */
-public class TesrComponent<T extends AEBaseTile> implements PreInitComponent {
+public class TesrComponent<T extends TileEntity> implements PreInitComponent {
 
 	private final Class<T> tileEntityClass;
 

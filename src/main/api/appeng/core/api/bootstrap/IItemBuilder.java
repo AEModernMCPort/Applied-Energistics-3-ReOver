e1566fa1485c1751cec441b7ec36a59ccaa4a189
elix-x <Elix-x@users.noreply.github.com>
2017-05-30 21:44:53 +0200

    Moved item def builder to new system

---
+++ src/main/api/appeng/core/api/bootstrap/IItemBuilder.java
@@ -0,0 +1,15 @@
+package appeng.core.api.bootstrap;
+
+import appeng.api.bootstrap.IDefinitionBuilder;
+import appeng.api.definitions.IItemDefinition;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public interface IItemBuilder<I extends Item, II extends IItemBuilder<I, II>> extends IDefinitionBuilder<I, IItemDefinition<I>, II> {
+
+	II creativeTab(CreativeTabs tab);
+
+	//TODO 1.11.2-ReOver - Be Back ?
+	//II rendering(ItemRenderingCustomizer callback);
+
+}
--- src/main/java/appeng/core/core/bootstrap/ItemDefinitionBuilder.java
+++ src/main/java/appeng/core/core/bootstrap/ItemDefinitionBuilder.java
@@ -1,7 +1,12 @@
-package appeng.core.lib.bootstrap_olde;
+package appeng.core.core.bootstrap;
 
+import appeng.api.bootstrap.DefinitionFactory;
 import appeng.api.definitions.IItemDefinition;
 import appeng.api.item.IStateItem;
+import appeng.core.api.bootstrap.IItemBuilder;
+import appeng.core.lib.bootstrap.DefinitionBuilder;
+import appeng.core.lib.bootstrap_olde.ItemRenderingCustomizer;
+import appeng.core.lib.bootstrap_olde.ItemSubDefinition;
 import appeng.core.lib.definitions.ItemDefinition;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
@@ -9,17 +14,16 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemDefinitionBuilder<I extends Item>
-		extends DefinitionBuilder<I, I, IItemDefinition<I>, ItemDefinitionBuilder<I>>
-		implements IItemBuilder<I, ItemDefinitionBuilder<I>> {
+public class ItemDefinitionBuilder<I extends Item> extends DefinitionBuilder<I, I, IItemDefinition<I>, ItemDefinitionBuilder<I>> implements IItemBuilder<I, ItemDefinitionBuilder<I>> {
 
-	@SideOnly(Side.CLIENT)
-	private ItemRendering itemRendering;
+	//TODO 1.11.2-ReOver - Be back?
+	/*@SideOnly(Side.CLIENT)
+	private ItemRendering itemRendering;*/
 
 	//TODO 1.11.2-ReOver - :P
 	private CreativeTabs creativeTab = CreativeTabs.REDSTONE;
 
-	ItemDefinitionBuilder(FeatureFactory factory, ResourceLocation registryName, I item){
+	ItemDefinitionBuilder(DefinitionFactory factory, ResourceLocation registryName, I item){
 		super(factory, registryName, item);
 		/*if(Platform.isClient()){
 			itemRendering = new ItemRendering();
@@ -32,19 +36,10 @@ public class ItemDefinitionBuilder<I extends Item>
 		return this;
 	}
 
-	@Override
-	public ItemDefinitionBuilder<I> rendering(ItemRenderingCustomizer callback){
-		/*if(Platform.isClient()){
-			customizeForClient(callback);
-		}*/
-
-		return this;
-	}
-
-	@SideOnly(Side.CLIENT)
+	/*@SideOnly(Side.CLIENT)
 	private void customizeForClient(ItemRenderingCustomizer callback){
 		callback.customize(itemRendering);
-	}
+	}*/
 
 	@Override
 	public IItemDefinition<I> def(I item){

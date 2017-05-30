22442113dc2765b9bd47f01d28dae36bf6cb6532
elix-x <Elix-x@users.noreply.github.com>
2017-05-30 15:37:23 +0200

    I think i've got it now

--- src/test/java/appeng/core/TypeExtractionTest.java
+++ src/test/java/appeng/core/TypeExtractionTest.java
@@ -11,15 +11,13 @@ public class TypeExtractionTest {
 
 	@Test
 	public void testGuavaTypeToken(){
-		System.out.println(this.genericMethod());
+		System.out.println(new TypeExtractor<String, Boolean>(){}.textract);
+		System.out.println(new TypeExtractor<String, Boolean>(){}.iextract);
 	}
 
-	public <T> Class genericMethod(){
-		return new TypeExtractor<T>(){}.extract.getRawType();
-	}
-
-	abstract class TypeExtractor<T> {
-		TypeToken<T> extract = new TypeToken<T>(getClass()){};
+	abstract class TypeExtractor<T, I> {
+		TypeToken<T> textract = new TypeToken<T>(getClass()){};
+		TypeToken<I> iextract = new TypeToken<I>(getClass()){};
 	}
 
 }

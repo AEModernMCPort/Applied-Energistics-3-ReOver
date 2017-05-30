f385168c32fe233bb52f68c63f5c75394241e64b
elix-x <Elix-x@users.noreply.github.com>
2017-05-30 22:15:15 +0200

    Finished moving core item defs to new system

--- src/main/java/appeng/core/core/definitions/CoreItemDefinitions.java
+++ src/main/java/appeng/core/core/definitions/CoreItemDefinitions.java
@@ -3,9 +3,12 @@ package appeng.core.core.definitions;
 import appeng.api.bootstrap.DefinitionFactory;
 import appeng.api.definitions.IItemDefinition;
 import appeng.core.AppEng;
+import appeng.core.api.bootstrap.IItemBuilder;
 import appeng.core.api.definitions.ICoreItemDefinitions;
+import appeng.core.core.bootstrap.ItemDefinitionBuilder;
 import appeng.core.item.ItemMaterial;
 import appeng.core.lib.definitions.Definitions;
+import appeng.core.lib.definitions.ItemDefinition;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 
@@ -14,9 +17,13 @@ public class CoreItemDefinitions extends Definitions<Item, IItemDefinition<Item>
 	private final IItemDefinition material;
 
 	public CoreItemDefinitions(DefinitionFactory registry){
-		this.material = registry.definitionBuilder(new ResourceLocation(AppEng.MODID, "material"), new ItemMaterial()).build();
+		this.material = registry.<Item, ItemDefinition<Item>, ItemDefinitionBuilder<Item>, Item>definitionBuilder(new ResourceLocation(AppEng.MODID, "material"), ih(new ItemMaterial())).build();
 
 		init(/*registry.buildDefaultItemBlocks()*/);
+	}
+
+	private DefinitionFactory.InputHandler<Item, Item> ih(Item item){
+		return new DefinitionFactory.InputHandler<Item, Item>(item){};
 	}
 
 }

6791b08ef0cfceb60be4872f80e6c97f53676a21
elix-x <Elix-x@users.noreply.github.com>
2017-05-30 17:51:18 +0200

    Implemented def factory ich

--- src/main/java/appeng/core/lib/bootstrap/DefinitionFactory.java
+++ src/main/java/appeng/core/lib/bootstrap/DefinitionFactory.java
@@ -1,10 +1,12 @@
 package appeng.core.lib.bootstrap;
 
 import appeng.api.bootstrap.IDefinitionBuilder;
+import appeng.api.bootstrap.InitializationComponentsHandler;
 import appeng.api.definitions.IDefinition;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.reflect.TypeToken;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -13,10 +15,19 @@ import java.util.function.BiFunction;
 
 public class DefinitionFactory implements appeng.api.bootstrap.DefinitionFactory {
 
+	private InitializationComponentsHandler commonInitHandler;
+	private InitializationComponentsHandler[] sidedInitHandlers;
 	private ImmutableMap<Pair<Class, Class>, BiFunction> definitionBuilderSuppliers;
 
-	public DefinitionFactory(Map<Pair<Class, Class>, BiFunction> definitionBuilderSuppliers){
-		this.definitionBuilderSuppliers = ImmutableMap.copyOf(definitionBuilderSuppliers);
+	public DefinitionFactory(InitializationComponentsHandler commonInitHandler, InitializationComponentsHandler[] sidedInitHandlers, ImmutableMap<Pair<Class, Class>, BiFunction> definitionBuilderSuppliers){
+		this.commonInitHandler = commonInitHandler;
+		this.sidedInitHandlers = sidedInitHandlers;
+		this.definitionBuilderSuppliers = definitionBuilderSuppliers;
+	}
+
+	@Override
+	public InitializationComponentsHandler initilizationHandler(Side side){
+		return side == null ? commonInitHandler : sidedInitHandlers[side.ordinal()];
 	}
 
 	private <T, D extends IDefinition<T>, B extends IDefinitionBuilder<T, D, B>, I> BiFunction<ResourceLocation, I, B> get(Pair<Class, Class> key){

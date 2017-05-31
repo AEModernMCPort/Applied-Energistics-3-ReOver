95c15954f946cf17c2950c82f8f6c6350b3ea44a
elix-x <Elix-x@users.noreply.github.com>
2017-05-31 23:25:57 +0200

    Removed notnulls as they don't work

--- src/main/api/appeng/api/bootstrap/SidedICHProxy.java
+++ src/main/api/appeng/api/bootstrap/SidedICHProxy.java
@@ -1,13 +1,9 @@
 package appeng.api.bootstrap;
 
-import javax.annotation.NotNull;
-
 public interface SidedICHProxy {
 
-	@NotNull
 	InitializationComponentsHandler client();
 
-	@NotNull
 	InitializationComponentsHandler server();
 
 }

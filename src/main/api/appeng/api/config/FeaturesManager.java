46d616f297f7f3d8d0a83b5c8fa3f6fa53972fab
elix-x <Elix-x@users.noreply.github.com>
2017-05-31 16:52:02 +0200

    Added features manager

---
+++ src/main/api/appeng/api/config/FeaturesManager.java
@@ -0,0 +1,31 @@
+package appeng.api.config;
+
+import net.minecraft.util.ResourceLocation;
+
+/**
+ * This class manages features availability - only enable/disable per individual feature. Features are represented with {@linkplain ResourceLocation}, {@linkplain ResourceLocation#resourceDomain} being the module id/name and {@linkplain ResourceLocation#resourcePath} being the feature path - file like path (separated with <tt>/</tt>) sequence of feature hierarchy.<br>
+ * A feature can have additional dependencies, but its' parent is always one of dependencies.<br>
+ * A feature is enabled if and only if all the features it depends on are enabled AND this feature itself is enabled.
+ */
+public interface FeaturesManager {
+
+	/**
+	 * A feature is enabled if and only if all the features it depends on are enabled AND this feature itself is enabled.
+	 *
+	 * @param feature The feature
+	 * @param def default value
+	 * @return Whether the feature is enabled
+	 */
+	boolean isEnabled(ResourceLocation feature, boolean def);
+
+	/**
+	 * Adds dependencies to the feature.
+	 *
+	 * @param feature The feature
+	 * @param deps Additional feature dependencies
+	 * @return <tt>this</tt>
+	 * @throws IllegalStateException if addition of given dependencies creates circularity
+	 */
+	FeaturesManager addDependencies(ResourceLocation feature, ResourceLocation... deps) throws IllegalStateException;
+
+}

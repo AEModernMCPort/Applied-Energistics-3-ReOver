3e61d6b2babe439c249f7d1f54a3fd3e428624e7
elix-x <Elix-x@users.noreply.github.com>
2017-05-31 21:59:29 +0200

    Added gson multimaps support

--- src/main/java/appeng/core/core/config/JSONConfigLoader.java
+++ src/main/java/appeng/core/core/config/JSONConfigLoader.java
@@ -2,14 +2,20 @@ package appeng.core.core.config;
 
 import appeng.core.lib.config.ConfigLoader;
 import code.elix_x.excomms.reflection.ReflectionHelper;
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+import com.google.common.collect.SetMultimap;
+import com.google.common.collect.Sets;
+import com.google.gson.*;
 import org.apache.commons.io.FileUtils;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class JSONConfigLoader<C> extends ConfigLoader<C> {
 
@@ -23,6 +29,30 @@ public class JSONConfigLoader<C> extends ConfigLoader<C> {
 		@Override
 		public boolean shouldSkipClass(Class<?> clazz){
 			return false;
+		}
+
+	}).registerTypeAdapter(Multimap.class, new JsonSerializer<Multimap>() {
+
+		public JsonElement serialize(Multimap multimap, Type type, JsonSerializationContext jsonSerializationContext){
+			return jsonSerializationContext.serialize(multimap.asMap());
+		}
+
+	}).registerTypeAdapter(Multimap.class, new JsonDeserializer<Multimap>() {
+
+		public Multimap deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
+				throws JsonParseException{
+			final SetMultimap<String, String> map = Multimaps.<String, String>newSetMultimap(new HashMap<String, Collection<String>>(), new com.google.common.base.Supplier<Set<String>>() {
+
+				public Set<String> get(){
+					return Sets.newHashSet();
+				}
+			});
+			for(Map.Entry<String, JsonElement> entry : ((JsonObject) jsonElement).entrySet()){
+				for(JsonElement element : (JsonArray) entry.getValue()){
+					map.get(entry.getKey()).add(element.getAsString());
+				}
+			}
+			return map;
 		}
 
 	}).create();

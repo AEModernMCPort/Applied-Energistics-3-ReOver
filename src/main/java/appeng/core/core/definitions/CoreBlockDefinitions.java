694aaf1ec44e15de6bc89b7f051a915a44d29928
elix-x <Elix-x@users.noreply.github.com>
2017-06-05 15:08:10 +0200

    Testing blocks, defaults & feature config

--- src/main/java/appeng/core/core/definitions/CoreBlockDefinitions.java
+++ src/main/java/appeng/core/core/definitions/CoreBlockDefinitions.java
@@ -2,14 +2,18 @@ package appeng.core.core.definitions;
 
 import appeng.api.bootstrap.DefinitionFactory;
 import appeng.api.definitions.IBlockDefinition;
+import appeng.core.AppEng;
+import appeng.core.api.bootstrap.IBlockBuilder;
 import appeng.core.api.definitions.ICoreBlockDefinitions;
 import appeng.core.lib.definitions.Definitions;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockGravel;
+import net.minecraft.util.ResourceLocation;
 
 public class CoreBlockDefinitions extends Definitions<Block, IBlockDefinition<Block>> implements ICoreBlockDefinitions {
 
 	public CoreBlockDefinitions(DefinitionFactory registry){
-
+		registry.<Block, IBlockDefinition, IBlockBuilder<Block, ?>, Block>definitionBuilder(new ResourceLocation(AppEng.MODID, "testgravel"), ih(new BlockGravel())).createDefaultItemBlock().build();
 	}
 
 	private DefinitionFactory.InputHandler<Block, Block> ih(Block block){

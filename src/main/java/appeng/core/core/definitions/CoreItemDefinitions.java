485fea8f9d733cf5d19ee6814e4045fa471af78d
elix-x <Elix-x@users.noreply.github.com>
2017-06-05 22:14:15 +0200

    Sided hidden lambas wooooork!!!

--- src/main/java/appeng/core/core/definitions/CoreItemDefinitions.java
+++ src/main/java/appeng/core/core/definitions/CoreItemDefinitions.java
@@ -1,21 +1,26 @@
 package appeng.core.core.definitions;
 
 import appeng.api.bootstrap.DefinitionFactory;
+import appeng.api.bootstrap.IDefinitionBuilder;
 import appeng.api.definitions.IItemDefinition;
 import appeng.core.AppEng;
 import appeng.core.api.bootstrap.IItemBuilder;
 import appeng.core.api.definitions.ICoreItemDefinitions;
+import appeng.core.core.AppEngCore;
 import appeng.core.item.ItemMaterial;
 import appeng.core.lib.definitions.Definitions;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.ItemModelMesherForge;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class CoreItemDefinitions extends Definitions<Item, IItemDefinition<Item>> implements ICoreItemDefinitions {
 
 	private final IItemDefinition material;
 
 	public CoreItemDefinitions(DefinitionFactory registry){
-		this.material = registry.<Item, IItemDefinition<Item>, IItemBuilder<Item, ?>, Item>definitionBuilder(new ResourceLocation(AppEng.MODID, "material"), ih(new ItemMaterial())).setFeature(null).build();
+		this.material = registry.<ItemMaterial, IItemDefinition<ItemMaterial>, IItemBuilder<ItemMaterial, ?>, Item>definitionBuilder(new ResourceLocation(AppEng.MODID, "material"), ih(new ItemMaterial())).setFeature(null).<IDefinitionBuilder.DefinitionInitializationComponent.Init<ItemMaterial, IItemDefinition<ItemMaterial>>>initializationComponent(Side.CLIENT, def -> Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(def.maybe().get(), stack -> def.maybe().get().getMaterial(stack).getModel(stack))).build();
 	}
 
 	private DefinitionFactory.InputHandler<Item, Item> ih(Item item){

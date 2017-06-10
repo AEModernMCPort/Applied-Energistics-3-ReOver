27684c0edd99f6ef5a8855b6104c353aa0def91b
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2017-06-10 16:23:55 +0200

    Cleaned up if

--- src/main/java/appeng/core/core/bootstrap/StateMapperComponent.java
+++ src/main/java/appeng/core/core/bootstrap/StateMapperComponent.java
@@ -23,8 +23,7 @@ public class StateMapperComponent implements IDefinitionBuilder.DefinitionInitia
 	@Override
 	public void init(IBlockDefinition<Block> def){
 		ModelLoader.setCustomStateMapper(def.maybe().get(), stateMapper);
-		if(stateMapper instanceof IResourceManagerReloadListener){
+		if(stateMapper instanceof IResourceManagerReloadListener)
 			((IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager()).registerReloadListener((IResourceManagerReloadListener) stateMapper);
-		}
 	}
 }

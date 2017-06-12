d01c139ece976f0bc9dae02862a300356990984a
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2017-06-12 17:24:16 +0200

    Reworked ModelOverrideComponent

--- src/main/java/appeng/core/core/client/bootstrap/ModelOverrideComponent.java
+++ src/main/java/appeng/core/core/client/bootstrap/ModelOverrideComponent.java
@@ -3,6 +3,7 @@ package appeng.core.core.client.bootstrap;
 import appeng.api.bootstrap.IDefinitionBuilder;
 import appeng.api.definitions.IDefinition;
 import appeng.core.AppEng;
+import appeng.core.core.AppEngCore;
 import com.google.common.collect.Sets;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -13,54 +14,22 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 /**
  * @author Fredi100
  */
 public class ModelOverrideComponent implements IDefinitionBuilder.DefinitionInitializationComponent {
 
-	static{
-		MinecraftForge.EVENT_BUS.register(ModelOverrideComponent.class);
-	}
+	private final Consumer<ModelBakeEvent> customizer;
 
-	// Maps from resource path to customizer
-	private final Map<String, BiFunction<ModelResourceLocation, IBakedModel, IBakedModel>> customizer = new HashMap<>();
-
-	public ModelOverrideComponent addOverride(String resourcePath, BiFunction<ModelResourceLocation, IBakedModel, IBakedModel> customizer){
-		this.customizer.put(resourcePath, customizer);
-		return this;
-	}
-
-	private static List<ModelOverrideComponent> instances = new ArrayList<ModelOverrideComponent>();
-
-	@SubscribeEvent
-	public static void sub(ModelBakeEvent event){
-		instances.forEach(instance -> instance.handleEvent(event));
-	}
-
-	void handleEvent(ModelBakeEvent event){
-		IRegistry<ModelResourceLocation, IBakedModel> modelRegistry = event.getModelRegistry();
-		Set<ModelResourceLocation> keys = Sets.newHashSet(modelRegistry.getKeys());
-
-		for(ModelResourceLocation location : keys){
-			if(!location.getResourceDomain().equals(AppEng.MODID)){
-				continue;
-			}
-
-			BiFunction<ModelResourceLocation, IBakedModel, IBakedModel> customizer = this.customizer.get(location.getResourcePath());
-			if(customizer != null){
-				IBakedModel orgModel = modelRegistry.getObject(location);
-				IBakedModel newModel = customizer.apply(location, orgModel);
-
-				if(newModel != orgModel){
-					modelRegistry.putObject(location, newModel);
-				}
-			}
-		}
+	public ModelOverrideComponent(Consumer<ModelBakeEvent> customizer){
+		this.customizer = customizer;
 	}
 
 	@Override
 	public void preInit(IDefinition def){
-		instances.add(this);
+		AppEngCore.proxy.acceptModelCustomizer(customizer);
 	}
+
 }
--- src/main/java/appeng/core/core/proxy/CoreClientProxy.java
+++ src/main/java/appeng/core/core/proxy/CoreClientProxy.java
@@ -1,10 +1,37 @@
 package appeng.core.core.proxy;
 
+import appeng.api.module.AEStateEvent;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class CoreClientProxy extends CoreProxy {
 
+	private List<Consumer<ModelBakeEvent>> modelCustomizers = new ArrayList<>();
+
 	public CoreClientProxy(){
 		super(Side.CLIENT);
+	}
+
+	@SubscribeEvent
+	public void sub(ModelBakeEvent event){
+		modelCustomizers.forEach(customizer -> customizer.accept(event));
+	}
+
+	@Override
+	public void preInit(AEStateEvent.AEPreInitializationEvent event){
+		super.preInit(event);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@Override
+	public void acceptModelCustomizer(Consumer<ModelBakeEvent> customizer){
+		super.acceptModelCustomizer(customizer);
+		modelCustomizers.add(customizer);
 	}
 }

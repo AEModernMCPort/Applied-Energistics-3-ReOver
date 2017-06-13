673224ec167e32635817aad058199f2f33edde1d
elix-x <Elix-x@users.noreply.github.com>
2017-06-13 12:46:25 +0200

    Added test item for default model testing

--- src/main/java/appeng/debug/definitions/DebugItemDefinitions.java
+++ src/main/java/appeng/debug/definitions/DebugItemDefinitions.java
@@ -2,12 +2,18 @@ package appeng.debug.definitions;
 
 import appeng.api.bootstrap.DefinitionFactory;
 import appeng.api.definitions.IItemDefinition;
+import appeng.core.AppEng;
+import appeng.core.api.bootstrap.IItemBuilder;
 import appeng.core.lib.definitions.Definitions;
+import appeng.debug.item.TestItem;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 
 public class DebugItemDefinitions extends Definitions<Item, IItemDefinition<Item>> {
 
 	public DebugItemDefinitions(DefinitionFactory registry){
+		registry.<Item, IItemDefinition<Item>, IItemBuilder<Item, ?>, Item>definitionBuilder(new ResourceLocation(AppEng.MODID, "test_item_model_default"), ih(new TestItem())).defaultModel().build();
+	}
 
 	private DefinitionFactory.InputHandler<Item, Item> ih(Item item){
 		return new DefinitionFactory.InputHandler<Item, Item>(item) {};

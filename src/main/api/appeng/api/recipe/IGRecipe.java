a03a4f7d69d08fb963e1378f4a342b5a6312e9d6
elix-x <Elix-x@users.noreply.github.com>
2017-06-27 15:36:04 +0200

    Added group to igrecipe

--- src/main/api/appeng/api/recipe/IGRecipe.java
+++ src/main/api/appeng/api/recipe/IGRecipe.java
@@ -2,4 +2,10 @@ package appeng.api.recipe;
 
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
-public interface IGRecipe<R extends IGRecipe<R>> extends IForgeRegistryEntry<R> {}
+public interface IGRecipe<R extends IGRecipe<R>> extends IForgeRegistryEntry<R> {
+
+	default String getGroup(){
+		return "";
+	}
+
+}

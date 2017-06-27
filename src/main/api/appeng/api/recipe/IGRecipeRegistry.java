8255defd55bf9ebcbcc83fe2ce755f0b2b16d43e
elix-x <Elix-x@users.noreply.github.com>
2017-06-27 18:00:58 +0200

    Default registry type

--- src/main/api/appeng/api/recipe/IGRecipeRegistry.java
+++ src/main/api/appeng/api/recipe/IGRecipeRegistry.java
@@ -3,6 +3,11 @@ package appeng.api.recipe;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
-public interface IGRecipeRegistry<R extends IGRecipe<R>> extends IForgeRegistryEntry<IGRecipeRegistry<R>>, IForgeRegistry<R> {
+public interface IGRecipeRegistry<R extends IGRecipe<R>> extends IForgeRegistryEntry<IGRecipeRegistry>, IForgeRegistry<R> {
 
+	@Override
+	default Class<IGRecipeRegistry> getRegistryType(){
+		return IGRecipeRegistry.class;
+	}
+	
 }

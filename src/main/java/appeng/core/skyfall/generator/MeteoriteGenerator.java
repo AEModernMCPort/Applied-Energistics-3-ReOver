374b5ea0ff734c346eff8f5f7392450bf08a1e1e
elix-x <Elix-x@users.noreply.github.com>
2017-06-28 23:28:04 +0200

    I'll just leave this here

---
+++ src/main/java/appeng/core/skyfall/generator/MeteoriteGenerator.java
@@ -0,0 +1,17 @@
+package appeng.core.skyfall.generator;
+
+import appeng.core.skyfall.api.generator.MutableBlockAccess;
+
+import java.util.Random;
+
+public class MeteoriteGenerator extends SkyobjectGenerator {
+
+	public MeteoriteGenerator(float defaultWeight){
+		super(defaultWeight);
+	}
+
+	@Override
+	public void generate(MutableBlockAccess world, Random random){
+
+	}
+}

ce6b70084e14ccc0ebf46003a9aa545ecf3ff349
dpeter99 <dpeter99@gmail.com>
2017-06-28 12:10:15 +0200

    Texture fixes

--- src/main/java/appeng/core/staticfire/block/CryoPod.java
+++ src/main/java/appeng/core/staticfire/block/CryoPod.java
@@ -2,6 +2,14 @@ package appeng.core.staticfire.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class CryoPod  extends Block implements StaticFireBlockBase{
 
@@ -9,6 +17,23 @@ public class CryoPod  extends Block implements StaticFireBlockBase{
 
     public CryoPod() {
         super(Material.ROCK);
+
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.TRANSLUCENT;
+    }
+
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
     }
 
     @Override

50ee3fa482266812f768253fc5771427a033e680
elix-x <Elix-x@users.noreply.github.com>
2017-06-28 22:36:05 +0200

    Futher matrices testing

--- src/main/java/appeng/debug/item/TestItem.java
+++ src/main/java/appeng/debug/item/TestItem.java
@@ -2,6 +2,8 @@ package appeng.debug.item;
 
 import appeng.core.lib.world.MutableBlockAccessWorldDelegate;
 import appeng.core.lib.world.OriginTransformingMutableBlockAccess;
+import appeng.core.lib.world.TransformedMutableBlockAccessM4f;
+import appeng.core.lib.world.TransformingMutableBlockAccess;
 import appeng.core.skyfall.api.generator.MutableBlockAccess;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -11,22 +13,22 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.joml.Matrix4f;
 
 public class TestItem extends Item {
 
 	@Override
 	public EnumActionResult onItemUse(EntityPlayer player, World oldworld, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
-		MutableBlockAccess world = new MutableBlockAccessWorldDelegate(oldworld);
-		world = new OriginTransformingMutableBlockAccess(world, pos);
-		for(int x = 0; x < 3; x++){
+		MutableBlockAccess wworld = new MutableBlockAccessWorldDelegate(oldworld);
+		TransformingMutableBlockAccess world = new OriginTransformingMutableBlockAccess(wworld, pos);
+		world = new TransformedMutableBlockAccessM4f(world, new Matrix4f().translate(-2, -1, 0).scale(2, 3, 1));
+		for(int x = 0; x < 5; x++){
 			for(int y = 0; y < 3; y++){
 				for(int z = 0; z < 3; z++){
 					world.setBlockState(new BlockPos(x, y, z), Blocks.STONE.getDefaultState());
 				}
 			}
 		}
-		System.out.println(pos);
-		System.out.println(new OriginTransformingMutableBlockAccess(world, pos).transform(pos));
 		return EnumActionResult.SUCCESS;
 	}
 

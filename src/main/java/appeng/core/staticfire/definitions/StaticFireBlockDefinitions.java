2c4dfa413058cd6e979624556594781c3699054b
dpeter99 <dpeter99@gmail.com>
2017-06-28 22:53:57 +0200

    Trying out the item model/meta data

--- src/main/java/appeng/core/staticfire/definitions/StaticFireBlockDefinitions.java
+++ src/main/java/appeng/core/staticfire/definitions/StaticFireBlockDefinitions.java
@@ -7,6 +7,8 @@ import appeng.api.definitions.IDefinitions;
 import appeng.api.definitions.IItemDefinition;
 import appeng.core.AppEng;
 import appeng.core.api.bootstrap.BlockItemCustomizer;
+import appeng.core.api.bootstrap.IBlockBuilder;
+import appeng.core.api.bootstrap.IItemBuilder;
 import appeng.core.core.bootstrap.BlockDefinitionBuilder;
 import appeng.core.core.client.bootstrap.ItemMeshDefinitionComponent;
 import appeng.core.lib.definitions.BlockDefinition;
@@ -23,6 +25,9 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 
 public class StaticFireBlockDefinitions extends Definitions<Block, IBlockDefinition<Block>> implements IStaticFireBlockDefinitions{
+
+    private final IBlockDefinition skyblock
+
 
     public StaticFireBlockDefinitions(DefinitionFactory registry){
 
@@ -43,7 +48,10 @@ public class StaticFireBlockDefinitions extends Definitions<Block, IBlockDefinit
 
         Block item = new SkyBlock();
 
-        registry.definitionBuilder(new ResourceLocation(AppEng.MODID, "sky_stone", ih(item)).<ItemBlock, BlockItemCustomizer.UseDefaultItemCustomize>createItem(builder -> builder.<IDefinitionBuilder.DefinitionInitializationComponent.PreInit<ItemBlock, IItemDefinition<ItemBlock>>>initializationComponent(Side.CLIENT, def -> new ItemMeshDefinitionComponent.BlockStateMapper2ItemMeshDefinition.createByMetadata(Block.getBlockFromItem(def.maybe().get())))));
+        skyblock = registry.<Block, IBlockDefinition<Block>, IBlockBuilder<Block, ?>, Block>definitionBuilder(new ResourceLocation(AppEng.MODID, "sky_stone"), ih(item))
+                .<ItemBlock, BlockItemCustomizer.UseDefaultItemCustomize>createItem(
+                        builder -> ((IItemBuilder<ItemBlock>) builder).<IDefinitionBuilder.DefinitionInitializationComponent.PreInit<ItemBlock, IItemDefinition<ItemBlock>>>initializationComponent(
+                                Side.CLIENT, def -> new ItemMeshDefinitionComponent.BlockStateMapper2ItemMeshDefinition.createByMetadata(Block.getBlockFromItem(def.maybe().get()))));
 
     }
 

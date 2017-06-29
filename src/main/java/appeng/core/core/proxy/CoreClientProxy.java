83de15120f92dfd51224ddbac275874cdf00d1bb
elix-x <Elix-x@users.noreply.github.com>
2017-06-29 22:25:59 +0200

    Working on material models

--- src/main/java/appeng/core/core/proxy/CoreClientProxy.java
+++ src/main/java/appeng/core/core/proxy/CoreClientProxy.java
@@ -1,8 +1,11 @@
 package appeng.core.core.proxy;
 
 import appeng.api.module.AEStateEvent;
+import appeng.core.core.AppEngCore;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -14,8 +17,6 @@ import java.util.function.Consumer;
 
 public class CoreClientProxy extends CoreProxy {
 
-
-
 	public CoreClientProxy(){
 		super(Side.CLIENT);
 	}
@@ -24,6 +25,14 @@ public class CoreClientProxy extends CoreProxy {
 	public void preInit(AEStateEvent.AEPreInitializationEvent event){
 		MinecraftForge.EVENT_BUS.register(this);
 		super.preInit(event);
+
+		modelRegisterers.add(() -> AppEngCore.INSTANCE.getMaterialRegistry().forEach(material -> {
+			try{
+				ModelLoaderRegistry.getModel(material.getModel());
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+		}));
 	}
 
 	private List<Runnable> modelRegisterers = new ArrayList<>();

8d974301db1e8a30c1236364b209cb38b7de48a4
elix-x <Elix-x@users.noreply.github.com>
2017-06-29 11:39:08 +0200

    Implemented block state 2 string serializer

---
+++ src/main/java/appeng/core/lib/util/BlockState2String.java
@@ -0,0 +1,42 @@
+package appeng.core.lib.util;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.InvalidBlockStateException;
+import net.minecraft.command.NumberInvalidException;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class BlockState2String {
+
+	public static final String FORMAT = "%1$s{%2$s}";
+	public static final String PROPFORMAT = "%1$s=%2$s";
+
+	public static String toString(IBlockState state){
+		Block block = state.getBlock();
+		List<String> stateSs = new ArrayList<>();
+		for(Map.Entry<IProperty<?>, Comparable<?>> entry : state.getProperties().entrySet()){
+			Map.Entry<IProperty, Comparable> e = (Map.Entry) entry;
+			stateSs.add(String.format(PROPFORMAT, e.getKey().getName(), e.getKey().getName(e.getValue())));
+		}
+		return String.format(FORMAT, block.getRegistryName(), String.join(",", stateSs));
+	}
+
+	public static IBlockState fromString(String s){
+		Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(s.split("\\{")[0]));
+		if(block == null) throw new IllegalArgumentException("Invalid block state string \"" + s + "\". Block not found.");
+		try {
+			String state = s.contains("{") ? s.split("\\{")[1].replace("}", "") : "";
+			return state.isEmpty() ? block.getDefaultState() : CommandBase.convertArgToBlockState(block, state);
+		} catch(NumberInvalidException | InvalidBlockStateException e){
+			throw new IllegalArgumentException("Invalid block state string \"" + s + "\". State could not be parsed.", e);
+		}
+	}
+
+}

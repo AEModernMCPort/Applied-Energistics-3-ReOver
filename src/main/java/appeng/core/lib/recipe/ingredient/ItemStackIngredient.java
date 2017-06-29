f3674ae90591cb5b545941a45f9db687aa940060
Alfred Emsenhuber <fredi.emsenhuber@live.at>
2017-06-29 16:07:41 +0200

    Set itemCount to 1 in EquivalentStack

--- src/main/java/appeng/core/lib/recipe/ingredient/ItemStackIngredient.java
+++ src/main/java/appeng/core/lib/recipe/ingredient/ItemStackIngredient.java
@@ -60,6 +60,7 @@ public interface ItemStackIngredient extends IGIngredient<ItemStack> {
 
 		public EquivalentStack(ItemStack itemstack){
 			this.itemstack = itemstack;
+			itemstack.setCount(1);
 		}
 
 		@Override

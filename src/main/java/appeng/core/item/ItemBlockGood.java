c2dd0bb9234f3f95187fd727e5f9646d6c34c8f1
elix-x <Elix-x@users.noreply.github.com>
2017-06-30 22:32:22 +0200

    Type parametrized good item block

--- src/main/java/appeng/core/item/ItemBlockGood.java
+++ src/main/java/appeng/core/item/ItemBlockGood.java
@@ -3,12 +3,17 @@ package appeng.core.item;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 
-public class ItemBlockGood extends ItemBlock {
+public class ItemBlockGood<B extends Block> extends ItemBlock {
 
-	public ItemBlockGood(Block block){
+	public ItemBlockGood(B block){
 		super(block);
 		setMaxDamage(0);
 		if(!block.getBlockState().getProperties().isEmpty()) setHasSubtypes(true);
+	}
+
+	@Override
+	public B getBlock(){
+		return (B) super.getBlock();
 	}
 
 	@Override

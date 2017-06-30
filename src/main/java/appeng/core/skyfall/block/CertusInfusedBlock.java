e60641ae4af5b6e769b1a0ffb81eeb70092f0fae
elix-x <Elix-x@users.noreply.github.com>
2017-06-30 23:27:28 +0200

    WTH??? Y u no work?
    Makes no sense.
    Copy pasted and still does not work.
    
    java.lang.IllegalArgumentException: Cannot set property
    PropertyEnum{name=variant, clazz=class
    appeng.core.skyfall.block.CertusInfusedBlock$CertusInfusedPropertyEnum,
    values=[STONE]} as it does not exist in
    BlockStateContainer{block=appliedenergistics3:certus_infused,
    properties=[variant]}

--- src/main/java/appeng/core/skyfall/block/CertusInfusedBlock.java
+++ src/main/java/appeng/core/skyfall/block/CertusInfusedBlock.java
@@ -1,72 +1,98 @@
 package appeng.core.skyfall.block;
 
-import appeng.core.lib.util.BlockState2String;
-import appeng.core.lib.util.OptionalUtil;
 import appeng.core.skyfall.config.SkyfallConfig;
-import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyHelper;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.NonNullList;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
 
 public class CertusInfusedBlock extends Block {
 
 	private static SkyfallConfig.Meteorite config = new SkyfallConfig.Meteorite();
 
-	public static final CertusInfusedProperty BLOCK = new CertusInfusedProperty("block");
+//	public static final CertusInfusedProperty VARIANT = new CertusInfusedProperty("block");
+	public static final IProperty<CertusInfusedPropertyEnum> VARIANT = PropertyEnum.create("variant", CertusInfusedPropertyEnum.class);
+
 
 	public static void recompile(SkyfallConfig.Meteorite config){
 		CertusInfusedBlock.config = config;
-		BLOCK.recompile();
 	}
 
 	public CertusInfusedBlock(){
 		super(Material.GROUND);
+		setCreativeTab(CreativeTabs.DECORATIONS);
 	}
 
 	@Override
 	public BlockStateContainer getBlockState(){
-		return new BlockStateContainer(this, BLOCK);
+		return new BlockStateContainer(this, VARIANT);
 	}
 
-	@Override
+	/*@Override
 	public IBlockState getStateFromMeta(int meta){
-		return super.getStateFromMeta(meta).withProperty(BLOCK, BLOCK.states.get(meta));
+		return super.getStateFromMeta(meta).withProperty(VARIANT, VARIANT.states.get(meta));
 	}
 
 	@Override
 	public int getMetaFromState(IBlockState state){
-		return BLOCK.states.indexOf(state.getValue(BLOCK));
+		return VARIANT.states.indexOf(state.getValue(VARIANT));
+	}*/
+
+	@Override
+	public IBlockState getStateFromMeta(int meta){
+		return super.getStateFromMeta(meta).withProperty(VARIANT, CertusInfusedPropertyEnum.values()[meta]);
 	}
 
 	@Override
+	public int getMetaFromState(IBlockState state){
+		return state.getValue(VARIANT).ordinal();
+	}
+
+	/*@Override
 	public int damageDropped(IBlockState state){
 		return getMetaFromState(state);
-	}
+	}*/
+
+	/*@Override
+	public void getSubBlocks(CreativeTabs itemIn, NonNullList<ItemStack> items){
+		for(int i = 0; i < VARIANT.states.size(); i++) items.add(new ItemStack(this, 1, i));
+	}*/
 
 	@Override
 	public void getSubBlocks(CreativeTabs itemIn, NonNullList<ItemStack> items){
-		for(int i = 0; i < BLOCK.states.size(); i++) items.add(new ItemStack(this, 1, i));
+		for(int i = 0; i < CertusInfusedPropertyEnum.values().length; i++) items.add(new ItemStack(this, 1, i));
 	}
 
 	public String getDisplayName(IBlockState state){
-		IBlockState pState = state.getValue(BLOCK).state;
+		IBlockState pState = state.getValue(VARIANT).state;
 		return Item.getItemFromBlock(pState.getBlock()).getItemStackDisplayName(new ItemStack(pState.getBlock(), 1, pState.getBlock().getMetaFromState(state)));
 	}
 
-	public static class CertusInfusedProperty extends PropertyHelper<CertusInfusedProperty.IBlockStateWrapper> {
+	public enum CertusInfusedPropertyEnum implements IStringSerializable {
+
+		STONE(Blocks.STONE.getDefaultState());
+
+		public IBlockState state;
+
+		CertusInfusedPropertyEnum(IBlockState state){
+			this.state = state;
+		}
+
+		@Override
+		public String getName(){
+			return name().toLowerCase();
+		}
+	}
+
+	/*public static class CertusInfusedProperty extends PropertyHelper<CertusInfusedProperty.IBlockStateWrapper> {
 
 		private ImmutableList<IBlockStateWrapper> states;
 
@@ -110,6 +136,6 @@ public class CertusInfusedBlock extends Block {
 			}
 
 		}
-	}
+	}*/
 
 }

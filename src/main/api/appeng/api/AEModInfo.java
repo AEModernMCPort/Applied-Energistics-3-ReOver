ec631b6df5b71bd9e9ebe7024aa65c5ab10c6a1e
elix-x <Elix-x@users.noreply.github.com>
2017-06-30 19:03:33 +0200

    Created AEModInfo to expose AE modid, name & version to API

---
+++ src/main/api/appeng/api/AEModInfo.java
@@ -0,0 +1,10 @@
+package appeng.api;
+
+
+public class AEModInfo {
+
+	public static final String MODID = "appliedenergistics3";
+	public static final String NAME = "Applied Energistics 3";
+	public static final String VERSION = "${version}";
+
+}

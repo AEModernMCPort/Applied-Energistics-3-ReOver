1de1beffcb05b7f834afe73ad0837245dd0653bd
elix-x <Elix-x@users.noreply.github.com>
2017-06-30 19:06:28 +0200

    Exposed skystone to API.

--- src/main/api/appeng/core/api/definitions/ICoreBlockDefinitions.java
+++ src/main/api/appeng/core/api/definitions/ICoreBlockDefinitions.java
@@ -1,9 +1,15 @@
 package appeng.core.api.definitions;
 
+import appeng.api.AEModInfo;
 import appeng.api.definitions.IBlockDefinition;
 import appeng.api.definitions.IDefinitions;
 import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
 
 public interface ICoreBlockDefinitions extends IDefinitions<Block, IBlockDefinition<Block>> {
+
+	default IBlockDefinition<Block> skystone(){
+		return get(new ResourceLocation(AEModInfo.MODID, "skystone"));
+	}
 
 }

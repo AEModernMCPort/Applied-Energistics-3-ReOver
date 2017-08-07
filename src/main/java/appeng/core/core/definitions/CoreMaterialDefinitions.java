93a0130739b2e0b8dfebf2896a68ca1c88b5b9d6
elix-x <Elix-x@users.noreply.github.com>
2017-08-07 20:25:26 +0200

    This branch certainly uncertus

--- src/main/java/appeng/core/core/definitions/CoreMaterialDefinitions.java
+++ src/main/java/appeng/core/core/definitions/CoreMaterialDefinitions.java
@@ -11,8 +11,17 @@ import net.minecraft.util.ResourceLocation;
 
 public class CoreMaterialDefinitions extends Definitions<Material, IMaterialDefinition<Material>> implements ICoreMaterialDefinitions {
 
+	private final IMaterialDefinition certusQuartz;
+	private final IMaterialDefinition certusRedstone;
+	private final IMaterialDefinition certusSulfur;
+	private final IMaterialDefinition incertus;
+
 	public CoreMaterialDefinitions(DefinitionFactory registry){
 		IMaterialDefinition<Material> m = registry.<Material, IMaterialDefinition<Material>, IMaterialBuilder<Material, ?>, Material>definitionBuilder(new ResourceLocation(AppEng.MODID, "invalid"), ih(new Material())).setFeature(null).build();
+		certusQuartz = registry.<Material, IMaterialDefinition<Material>, IMaterialBuilder<Material, ?>, Material>definitionBuilder(new ResourceLocation(AppEng.MODID, "certus_quartz"), ih(new Material())).build();
+		certusRedstone = registry.<Material, IMaterialDefinition<Material>, IMaterialBuilder<Material, ?>, Material>definitionBuilder(new ResourceLocation(AppEng.MODID, "certus_redstone"), ih(new Material())).build();
+		certusSulfur = registry.<Material, IMaterialDefinition<Material>, IMaterialBuilder<Material, ?>, Material>definitionBuilder(new ResourceLocation(AppEng.MODID, "certus_sulfur"), ih(new Material())).build();
+		incertus = registry.<Material, IMaterialDefinition<Material>, IMaterialBuilder<Material, ?>, Material>definitionBuilder(new ResourceLocation(AppEng.MODID, "incertus"), ih(new Material())).build();
 	}
 
 	private DefinitionFactory.InputHandler<Material, Material> ih(Material material){

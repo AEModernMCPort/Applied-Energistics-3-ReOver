1062fe1aa99eb3cbd5648e0c79f638f15be0bb24
elix-x <Elix-x@users.noreply.github.com>
2017-08-08 11:52:26 +0200

    Added new materials to core mat defs interface

--- src/main/api/appeng/core/core/api/definitions/ICoreMaterialDefinitions.java
+++ src/main/api/appeng/core/core/api/definitions/ICoreMaterialDefinitions.java
@@ -1,9 +1,27 @@
 package appeng.core.core.api.definitions;
 
+import appeng.api.AEModInfo;
 import appeng.api.definitions.IDefinitions;
 import appeng.core.core.api.definition.IMaterialDefinition;
 import appeng.core.core.api.material.Material;
+import net.minecraft.util.ResourceLocation;
 
 public interface ICoreMaterialDefinitions extends IDefinitions<Material, IMaterialDefinition<Material>> {
+
+	default IMaterialDefinition<Material> certusQuartz(){
+		return get(new ResourceLocation(AEModInfo.MODID, "certus_quartz"));
+	}
+
+	default IMaterialDefinition<Material> certusRedstone(){
+		return get(new ResourceLocation(AEModInfo.MODID, "certus_redstone"));
+	}
+
+	default IMaterialDefinition<Material> certusSulfur(){
+		return get(new ResourceLocation(AEModInfo.MODID, "certus_sulfur"));
+	}
+
+	default IMaterialDefinition<Material> incertus(){
+		return get(new ResourceLocation(AEModInfo.MODID, "incertus"));
+	}
 
 }

53d66c4b378796e5ed78af06b1ad9c0fc8f93a9e
elix-x <Elix-x@users.noreply.github.com>
2017-08-08 15:54:53 +0200

    Automatically remap fluid block model
    [->andromeda]

--- src/main/java/appeng/core/core/bootstrap/FluidDefinitionBuilder.java
+++ src/main/java/appeng/core/core/bootstrap/FluidDefinitionBuilder.java
@@ -2,18 +2,25 @@ package appeng.core.core.bootstrap;
 
 import appeng.api.bootstrap.DefinitionFactory;
 import appeng.core.core.api.bootstrap.FluidBlockCustomizer;
+import appeng.core.core.api.bootstrap.IBlockBuilder;
 import appeng.core.core.api.bootstrap.IFluidBuilder;
 import appeng.core.core.api.definition.IBlockDefinition;
 import appeng.core.core.api.definition.IFluidDefinition;
+import appeng.core.core.client.bootstrap.StateMapperComponent;
+import appeng.core.core.client.statemap.StaticStateMapper;
 import appeng.core.core.definition.FluidDefinition;
 import appeng.core.lib.bootstrap.DefinitionBuilder;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.IFluidBlock;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.relauncher.Side;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 public class FluidDefinitionBuilder<F extends Fluid> extends DefinitionBuilder<F, F, IFluidDefinition<F>, FluidDefinitionBuilder<F>> implements IFluidBuilder<F, FluidDefinitionBuilder<F>> {
@@ -21,7 +28,7 @@ public class FluidDefinitionBuilder<F extends Fluid> extends DefinitionBuilder<F
 	private Function<IFluidDefinition<F>, IBlockDefinition<?>> block;
 
 	public FluidDefinitionBuilder(DefinitionFactory factory, ResourceLocation registryName, F fluid){
-		super(factory, new ResourceLocation(Loader.instance().activeModContainer().getName(), fluid.getName()), fluid, "fluid");
+		super(factory, new ResourceLocation(Loader.instance().activeModContainer().getModId(), fluid.getName()), fluid, "fluid");
 	}
 
 	@Override
@@ -32,7 +39,7 @@ public class FluidDefinitionBuilder<F extends Fluid> extends DefinitionBuilder<F
 
 	@Override
 	public <B extends Block & IFluidBlock> FluidDefinitionBuilder<F> createBlock(FluidBlockCustomizer<F, B> customizer){
-		return setBlock(def -> customizer.customize(factory.definitionBuilder(registryName, fluidBlockIh(customizer.createBlock(def)))).setFeature(feature).build());
+		return setBlock(def -> customizer.customize(factory.<B, IBlockDefinition<B>, IBlockBuilder<B, ?>, Block>definitionBuilder(registryName, fluidBlockIh(customizer.createBlock(def))).initializationComponent(Side.CLIENT, new StateMapperComponent<>(old -> Optional.of(new StaticStateMapper(new ModelResourceLocation(new ResourceLocation(ForgeVersion.MOD_ID, "fluid"), def.identifier().getResourcePath())))))).setFeature(feature).build());
 	}
 
 	@Override

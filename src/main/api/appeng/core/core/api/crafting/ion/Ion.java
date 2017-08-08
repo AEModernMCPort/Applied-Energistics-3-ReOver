834b606ea1e8cd12268403a19f95051bf265c1ed
elix-x <Elix-x@users.noreply.github.com>
2017-08-08 11:59:36 +0200

    Ions can now define environment color modifier

--- src/main/api/appeng/core/core/api/crafting/ion/Ion.java
+++ src/main/api/appeng/core/core/api/crafting/ion/Ion.java
@@ -1,5 +1,12 @@
 package appeng.core.core.api.crafting.ion;
 
+import code.elix_x.excomms.color.RGBA;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
-public interface Ion extends IForgeRegistryEntry<Ion> {}
+public interface Ion extends IForgeRegistryEntry<Ion> {
+
+	default RGBA getColorModifier(){
+		return new RGBA(1f, 1f, 1f, 1f);
+	}
+
+}

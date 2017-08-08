666082230aaf0111030a7072bbb8b826f93ab5bd
elix-x <Elix-x@users.noreply.github.com>
2017-08-08 21:46:21 +0200

    Attaching caps to ion mats using single caps provider

--- src/main/java/appeng/core/core/material/IonMaterial.java
+++ src/main/java/appeng/core/core/material/IonMaterial.java
@@ -6,27 +6,22 @@ import appeng.core.core.api.crafting.ion.IonProvider;
 import appeng.core.core.api.item.IItemMaterial;
 import appeng.core.core.api.material.Material;
 import appeng.core.core.crafting.ion.IonProviderImpl;
+import appeng.core.lib.capability.SingleCapabilityProvider;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 @Mod.EventBusSubscriber(modid = AppEng.MODID)
-public class IonMaterial extends Material implements ICapabilityProvider {
+public class IonMaterial extends Material {
 
 	@SubscribeEvent
 	public static void attachItemCaps(AttachCapabilitiesEvent<ItemStack> event){
 		if(event.getObject().getItem() instanceof IItemMaterial){
 			Material material = ((IItemMaterial) event.getObject().getItem()).getMaterial(event.getObject());
-			if(material instanceof IonMaterial) event.addCapability(new ResourceLocation(AppEng.MODID, "ion_provider"), (IonMaterial) material);
+			if(material instanceof IonMaterial) event.addCapability(new ResourceLocation(AppEng.MODID, "ion_provider"), new SingleCapabilityProvider<>(AppEngCore.ionProviderCapability, ((IonMaterial) material).ionProvider));
 		}
 	}
 
@@ -46,17 +41,6 @@ public class IonMaterial extends Material implements ICapabilityProvider {
 
 	public void findIonBySameRegName(int amount){
 		setIonProvider(new IonProviderImpl(AppEngCore.INSTANCE.getIonRegistry().getValue(getRegistryName()), amount));
-	}
-
-	@Override
-	public boolean hasCapability(@Nonnull Capability<?> capability, @Nullable EnumFacing facing){
-		return capability == AppEngCore.ionProviderCapability;
-	}
-
-	@Nullable
-	@Override
-	public <T> T getCapability(@Nonnull Capability<T> capability, @Nullable EnumFacing facing){
-		return capability == AppEngCore.ionProviderCapability ? (T) ionProvider : null;
 	}
 
 	@Override

acdf3c58d39b6223c50ac6b318952adc85f6ac10
elix-x <Elix-x@users.noreply.github.com>
2017-08-08 18:08:38 +0200

    Added update stuff to ion env tile

--- src/main/java/appeng/core/core/tile/IonEnvironmentTile.java
+++ src/main/java/appeng/core/core/tile/IonEnvironmentTile.java
@@ -3,6 +3,8 @@ package appeng.core.core.tile;
 import appeng.core.core.AppEngCore;
 import appeng.core.core.crafting.ion.IonEnvironment;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
@@ -45,4 +47,21 @@ public class IonEnvironmentTile extends TileEntity {
 		compound.setTag("environment", this.environment.serializeNBT());
 		return compound;
 	}
+
+	@Override
+	public NBTTagCompound getUpdateTag(){
+		return serializeNBT();
+	}
+
+	@Nullable
+	@Override
+	public SPacketUpdateTileEntity getUpdatePacket(){
+		return new SPacketUpdateTileEntity(pos, 0, getUpdateTag());
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt){
+		deserializeNBT(pkt.getNbtCompound());
+	}
+
 }

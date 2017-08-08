90bc7473d1476ead24ec2ce0cc54e2beeb80c514
elix-x <Elix-x@users.noreply.github.com>
2017-08-08 11:27:56 +0200

    Added tile caps methods

--- src/main/java/appeng/core/core/tile/IonEnvironmentTile.java
+++ src/main/java/appeng/core/core/tile/IonEnvironmentTile.java
@@ -1,10 +1,15 @@
 package appeng.core.core.tile;
 
+import appeng.core.core.AppEngCore;
 import appeng.core.core.crafting.ion.IonEnvironment;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fluids.Fluid;
+
+import javax.annotation.Nullable;
 
 public class IonEnvironmentTile extends TileEntity {
 
@@ -15,6 +20,17 @@ public class IonEnvironmentTile extends TileEntity {
 
 	public IonEnvironmentTile(Fluid fluid){
 		this.environment.setEnvironment(fluid);
+	}
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing){
+		return capability == AppEngCore.ionEnvironmentCapability || super.hasCapability(capability, facing);
+	}
+
+	@Nullable
+	@Override
+	public <T> T getCapability(Capability<T> capability, @Nullable EnumFacing facing){
+		return capability == AppEngCore.ionEnvironmentCapability ? (T) environment : super.getCapability(capability, facing);
 	}
 
 	@Override

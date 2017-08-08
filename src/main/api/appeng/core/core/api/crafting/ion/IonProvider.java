c96c358b4480d3f9c8305992a255e39602d49370
elix-x <Elix-x@users.noreply.github.com>
2017-08-08 12:15:47 +0200

    Made ion provider fluid sensitive

--- src/main/api/appeng/core/core/api/crafting/ion/IonProvider.java
+++ src/main/api/appeng/core/core/api/crafting/ion/IonProvider.java
@@ -1,12 +1,14 @@
 package appeng.core.core.api.crafting.ion;
 
+import net.minecraftforge.fluids.Fluid;
+
 import java.util.Map;
 
 public interface IonProvider {
 
 	Map<Ion, Integer> getIons();
 
-	default boolean isReactive(){
+	default boolean isReactive(Fluid fluid){
 		return false;
 	}
 

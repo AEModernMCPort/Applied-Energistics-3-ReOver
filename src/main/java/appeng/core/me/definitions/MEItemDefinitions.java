abec613c787938c55b7f823e2339dcfa511d6b0d
dpeter99 <dpeter99@gmail.com>
2017-08-09 16:42:09 +0200

    fix

--- src/main/java/appeng/core/me/definitions/MEItemDefinitions.java
+++ src/main/java/appeng/core/me/definitions/MEItemDefinitions.java
@@ -3,10 +3,10 @@ package appeng.core.me.definitions;
 import appeng.api.bootstrap.DefinitionFactory;
 import appeng.core.core.api.definition.IItemDefinition;
 import appeng.core.lib.definitions.Definitions;
-import appeng.core.staticfire.api.definitions.IStaticFireItemDefinitions;
+import appeng.core.me.api.definitions.IMEItemDefinitions;
 import net.minecraft.item.Item;
 
-public class MEItemDefinitions extends Definitions<Item, IItemDefinition<Item>> implements IStaticFireItemDefinitions {
+public class MEItemDefinitions extends Definitions<Item, IItemDefinition<Item>> implements IMEItemDefinitions {
 
 	public MEItemDefinitions(DefinitionFactory registry){
 

c8c1c4c8eb049e00e43e94217cb6fb14e0dc0e4a
elix-x <Elix-x@users.noreply.github.com>
2017-08-09 18:39:08 +0200

    Created ion environment context

---
+++ src/main/api/appeng/core/core/api/crafting/ion/IonEnvironmentContext.java
@@ -0,0 +1,29 @@
+package appeng.core.core.api.crafting.ion;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+
+import java.util.Optional;
+
+public interface IonEnvironmentContext {
+
+	/**
+	 * Maybe the world of the environment
+	 * @return the world of the environment
+	 */
+	Optional<World> world();
+
+	/**
+	 * Maybe the position of the environment
+	 * @return the position of the environment
+	 */
+	Optional<BlockPos> pos();
+
+	/**
+	 * Maybe capabilities of the environment
+	 * @return capabilities of the environment
+	 */
+	Optional<ICapabilityProvider> capabilities();
+
+}

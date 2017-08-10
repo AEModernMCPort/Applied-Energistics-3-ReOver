f0384aed33bb9662b1768a7ffd181385408a8a54
elix-x <Elix-x@users.noreply.github.com>
2017-08-10 11:43:42 +0200

    Core config is now compileable & init listening
    [->andromeda]
    
    (cherry picked from commit 7567549eee5ffc7e509936be1d215b33804164e7)

--- src/main/java/appeng/core/core/AppEngCore.java
+++ src/main/java/appeng/core/core/AppEngCore.java
@@ -139,7 +139,7 @@ public class AppEngCore implements ICore {
 		} catch(IOException e){
 			logger.error("Caught exception loading configuration", e);
 		}
-		config = configLoader.configuration();
+		initHandler.accept(config = configLoader.configuration());
 
 		registry = event.factory(initHandler, proxy);
 		this.itemDefinitions = new CoreItemDefinitions(registry);

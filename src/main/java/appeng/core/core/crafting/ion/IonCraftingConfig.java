7c7b07bd3db9d5ff6e96d4608684c5017358f67a
elix-x <Elix-x@users.noreply.github.com>
2017-08-10 17:55:54 +0200

    RL does not have no-args contructor :(

--- src/main/java/appeng/core/core/crafting/ion/IonCraftingConfig.java
+++ src/main/java/appeng/core/core/crafting/ion/IonCraftingConfig.java
@@ -21,20 +21,20 @@ import java.util.stream.Collectors;
 
 public class IonCraftingConfig implements ConfigCompilable, InitializationComponent.Init  {
 
-	private Map<String, List<MutablePair<ResourceLocation, Integer>>> oreDictToIons = new HashMap<>();
+	private Map<String, List<MutablePair<String, Integer>>> oreDictToIons = new HashMap<>();
 	public transient Multimap<String, Pair<Ion, Integer>> oreDictToIonsM = HashMultimap.create();
 
 	public IonCraftingConfig(){
-		oreDictToIons.put("gemQuartz", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "quartz"), 1)));
-		oreDictToIons.put("dustRedstone", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "redstone"), 1)));
-		oreDictToIons.put("gunpowder", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "sulfur"), 1)));
-		oreDictToIons.put("dustSulfur", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "sulfur"), 1)));
-		oreDictToIons.put("enderpearl", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "ender"), 1)));
+		oreDictToIons.put("gemQuartz", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "quartz").toString(), 1)));
+		oreDictToIons.put("dustRedstone", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "redstone").toString(), 1)));
+		oreDictToIons.put("gunpowder", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "sulfur").toString(), 1)));
+		oreDictToIons.put("dustSulfur", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "sulfur").toString(), 1)));
+		oreDictToIons.put("enderpearl", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "ender").toString(), 1)));
 
-		oreDictToIons.put("certusQuartz", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "certus"), 1), new MutablePair<>(new ResourceLocation(AppEng.MODID, "quartz"), 3)));
-		oreDictToIons.put("certusRedstone", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "certus"), 1), new MutablePair<>(new ResourceLocation(AppEng.MODID, "redstone"), 3)));
-		oreDictToIons.put("certusSulfur", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "certus"), 1), new MutablePair<>(new ResourceLocation(AppEng.MODID, "sulfur"), 2)));
-		oreDictToIons.put("incertus", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "certus"), 1), new MutablePair<>(new ResourceLocation(AppEng.MODID, "ender"), 1)));
+		oreDictToIons.put("certusQuartz", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "certus").toString(), 1), new MutablePair<>(new ResourceLocation(AppEng.MODID, "quartz").toString(), 3)));
+		oreDictToIons.put("certusRedstone", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "certus").toString(), 1), new MutablePair<>(new ResourceLocation(AppEng.MODID, "redstone").toString(), 3)));
+		oreDictToIons.put("certusSulfur", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "certus").toString(), 1), new MutablePair<>(new ResourceLocation(AppEng.MODID, "sulfur").toString(), 2)));
+		oreDictToIons.put("incertus", Lists.newArrayList(new MutablePair<>(new ResourceLocation(AppEng.MODID, "certus").toString(), 1), new MutablePair<>(new ResourceLocation(AppEng.MODID, "ender").toString(), 1)));
 	}
 
 	@Override
@@ -45,13 +45,13 @@ public class IonCraftingConfig implements ConfigCompilable, InitializationCompon
 	@Override
 	public void init(){
 		oreDictToIonsM.clear();
-		oreDictToIons.forEach((ore, ions) -> ions.forEach(ionr -> Optional.ofNullable(AppEngCore.INSTANCE.getIonRegistry().getValue(ionr.getKey())).ifPresent(ion -> oreDictToIonsM.put(ore, new ImmutablePair<>(ion, ionr.getRight())))));
+		oreDictToIons.forEach((ore, ions) -> ions.forEach(ionr -> Optional.ofNullable(AppEngCore.INSTANCE.getIonRegistry().getValue(new ResourceLocation(ionr.getKey()))).ifPresent(ion -> oreDictToIonsM.put(ore, new ImmutablePair<>(ion, ionr.getRight())))));
 	}
 
 	@Override
 	public void decompile(){
 		oreDictToIons.clear();
-		oreDictToIonsM.keySet().forEach(ore -> oreDictToIons.put(ore, oreDictToIonsM.get(ore).stream().map(ion -> new MutablePair<>(ion.getLeft().getRegistryName(), ion.getRight())).collect(Collectors.toList())));
+		oreDictToIonsM.keySet().forEach(ore -> oreDictToIons.put(ore, oreDictToIonsM.get(ore).stream().map(ion -> new MutablePair<>(ion.getLeft().getRegistryName().toString(), ion.getRight())).collect(Collectors.toList())));
 	}
 
 }

737b3cfb828388a18ddb361d151b657765939dff
elix-x <Elix-x@users.noreply.github.com>
2017-08-16 18:00:13 +0200

    Removed unused GSON exclusion strategy

--- src/main/java/appeng/core/core/config/JSONConfigLoader.java
+++ src/main/java/appeng/core/core/config/JSONConfigLoader.java
@@ -3,7 +3,9 @@ package appeng.core.core.config;
 import appeng.api.config.ConfigCompilable;
 import appeng.core.lib.config.ConfigLoader;
 import code.elix_x.excomms.reflection.ReflectionHelper;
-import com.google.gson.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import net.minecraft.util.ResourceLocation;
@@ -15,19 +17,7 @@ import java.nio.charset.Charset;
 
 public class JSONConfigLoader<C> extends ConfigLoader<C> {
 
-	public static final Gson GSON = new GsonBuilder().addDeserializationExclusionStrategy(new ExclusionStrategy() {
-
-		@Override
-		public boolean shouldSkipField(FieldAttributes f){
-			return f.getName().contains("___");
-		}
-
-		@Override
-		public boolean shouldSkipClass(Class<?> clazz){
-			return false;
-		}
-
-	}).registerTypeAdapter(ResourceLocation.class, new TypeAdapter<ResourceLocation>() {
+	public static final Gson GSON = new GsonBuilder().registerTypeAdapter(ResourceLocation.class, new TypeAdapter<ResourceLocation>() {
 
 		@Override
 		public void write(JsonWriter out, ResourceLocation value) throws IOException{

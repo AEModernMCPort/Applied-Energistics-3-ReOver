131756bda60253a0981e4d4f2bfcf155a34ff7f1
elix-x <Elix-x@users.noreply.github.com>
2017-08-17 14:02:00 +0200

    Revert "Forgot that RL does not have n-args constructor"
    This reverts commit d35d01eef78e2bb36b8c1e43f70da2cb6ea3991d.

--- src/main/java/appeng/core/core/crafting/ion/IonCraftingConfig.java
+++ src/main/java/appeng/core/core/crafting/ion/IonCraftingConfig.java
@@ -131,38 +131,38 @@ public class IonCraftingConfig implements ConfigCompilable, InitializationCompon
 
 	public static class Recipe {
 
-		public List<MutablePair<String, Integer>> ions = new ArrayList<>();
+		public List<MutablePair<ResourceLocation, Integer>> ions = new ArrayList<>();
 		public List<Result> results = new ArrayList<>();
 
 		public Recipe(){
 		}
 
-		public Recipe(List<MutablePair<String, Integer>> ions, List<Result> results){
+		public Recipe(List<MutablePair<ResourceLocation, Integer>> ions, List<Result> results){
 			this.ions = ions;
 			this.results = results;
 		}
 
 		public Recipe.Compiled compile(){
-			return new Compiled(Lists.transform(ions, pair -> new ImmutablePair<>(AppEngCore.INSTANCE.getIonRegistry().getValue(new ResourceLocation(pair.getLeft())), pair.getRight())), Lists.transform(results, Result::compile));
+			return new Compiled(Lists.transform(ions, pair -> new ImmutablePair<>(AppEngCore.INSTANCE.getIonRegistry().getValue(pair.getLeft()), pair.getRight())), Lists.transform(results, Result::compile));
 		}
 
 		public static class Result {
 
-			public String type;
-			public String result;
+			public ResourceLocation type;
+			public ResourceLocation result;
 			public int amount;
 
 			public Result(){
 			}
 
-			public Result(String type, String result, int amount){
+			public Result(ResourceLocation type, ResourceLocation result, int amount){
 				this.type = type;
 				this.result = result;
 				this.amount = amount;
 			}
 
 			public <T> Result.Compiled<T> compile(){
-				return new Compiled<>(new ResourceLocation(type), AppEngCore.INSTANCE.getCraftingIonRegistry().deserializeResult(new ResourceLocation(type), new ResourceLocation(result)), amount);
+				return new Compiled<>(type, AppEngCore.INSTANCE.getCraftingIonRegistry().deserializeResult(type, result), amount);
 			}
 
 			public static class Compiled<T> {
@@ -178,7 +178,7 @@ public class IonCraftingConfig implements ConfigCompilable, InitializationCompon
 				}
 
 				public Result decompile(){
-					return new Result(type.toString(), AppEngCore.INSTANCE.getCraftingIonRegistry().serializeResult(type, result).toString(), amount);
+					return new Result(type, AppEngCore.INSTANCE.getCraftingIonRegistry().serializeResult(type, result), amount);
 				}
 
 			}
@@ -196,7 +196,7 @@ public class IonCraftingConfig implements ConfigCompilable, InitializationCompon
 			}
 
 			public Recipe decompile(){
-				return new Recipe(Lists.transform(ions, pair -> new MutablePair<>(pair.getLeft().getRegistryName().toString(), pair.getRight())), Lists.transform(results, Result.Compiled::decompile));
+				return new Recipe(Lists.transform(ions, pair -> new MutablePair<>(pair.getLeft().getRegistryName(), pair.getRight())), Lists.transform(results, Result.Compiled::decompile));
 			}
 
 		}

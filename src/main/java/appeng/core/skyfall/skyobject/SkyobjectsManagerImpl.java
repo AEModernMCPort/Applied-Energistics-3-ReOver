2187737898b7ea7075fe72d9bab55010a13beb77
elix-x <Elix-x@users.noreply.github.com>
2017-08-18 22:52:43 +0200

    Implemented skyobjects manager serialization

--- src/main/java/appeng/core/skyfall/skyobject/SkyobjectsManagerImpl.java
+++ src/main/java/appeng/core/skyfall/skyobject/SkyobjectsManagerImpl.java
@@ -3,13 +3,18 @@ package appeng.core.skyfall.skyobject;
 import appeng.core.AppEng;
 import appeng.core.lib.capability.SingleCapabilityProvider;
 import appeng.core.skyfall.AppEngSkyfall;
+import appeng.core.skyfall.api.skyobject.Skyobject;
 import appeng.core.skyfall.api.skyobject.SkyobjectsManager;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = AppEng.MODID)
 public class SkyobjectsManagerImpl implements SkyobjectsManager {
@@ -19,14 +24,26 @@ public class SkyobjectsManagerImpl implements SkyobjectsManager {
 		event.addCapability(new ResourceLocation(AppEng.MODID, "skyobjects_manager"), new SingleCapabilityProvider<>(AppEngSkyfall.skyobjectsManagerCapability, new SkyobjectsManagerImpl()));
 	}
 
+	protected List<Skyobject> skyobjects = new ArrayList<>();
+
 	@Override
 	public NBTTagCompound serializeNBT(){
-		return null;
+		NBTTagCompound nbt = new NBTTagCompound();
+		NBTTagList skyobjects = new NBTTagList();
+		this.skyobjects.forEach(skyobject -> {
+			NBTTagCompound tag = new NBTTagCompound();
+			tag.setString("id", skyobject.getProvider().getRegistryName().toString());
+			tag.setTag("data", skyobject.getProvider().serializeNBT(skyobject));
+			skyobjects.appendTag(tag);
+		});
+		nbt.setTag("skyobjects", skyobjects);
+		return nbt;
 	}
 
 	@Override
 	public void deserializeNBT(NBTTagCompound nbt){
-
+		NBTTagList skyobjects = nbt.getTagList("skyobjects", 10);
+		skyobjects.forEach(tag -> this.skyobjects.add(AppEngSkyfall.INSTANCE.getSkyobjectProvidersRegistry().getValue(new ResourceLocation(((NBTTagCompound) tag).getString("id"))).deserializeNBT(((NBTTagCompound) tag).getCompoundTag("data"))));
 	}
 
 }

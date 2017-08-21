8fe041d96a16ac58929dfc5786c93c11d0f44bc2
elix-x <Elix-x@users.noreply.github.com>
2017-08-21 22:56:24 +0200

    How about we actaully define skyobjects

--- src/main/java/appeng/core/skyfall/definitions/SkyfallSkyobjectProviderDefinitions.java
+++ src/main/java/appeng/core/skyfall/definitions/SkyfallSkyobjectProviderDefinitions.java
@@ -1,15 +1,21 @@
 package appeng.core.skyfall.definitions;
 
 import appeng.api.bootstrap.DefinitionFactory;
+import appeng.core.AppEng;
 import appeng.core.lib.definitions.Definitions;
+import appeng.core.skyfall.api.bootstrap.ISkyobjectGeneratorBuilder;
 import appeng.core.skyfall.api.definition.ISkyobjectProviderDefinition;
 import appeng.core.skyfall.api.definitions.ISkyfallSkyobjectProviderDefinitions;
 import appeng.core.skyfall.api.skyobject.SkyobjectProvider;
+import appeng.core.skyfall.skyobject.objects.MeteoriteProvider;
+import net.minecraft.util.ResourceLocation;
 
 public class SkyfallSkyobjectProviderDefinitions extends Definitions<SkyobjectProvider, ISkyobjectProviderDefinition<SkyobjectProvider>> implements ISkyfallSkyobjectProviderDefinitions {
 
+	private final ISkyobjectProviderDefinition meteorite;
+	
 	public SkyfallSkyobjectProviderDefinitions(DefinitionFactory registry){
-
+		meteorite = registry.<MeteoriteProvider, ISkyobjectProviderDefinition<MeteoriteProvider>, ISkyobjectGeneratorBuilder<MeteoriteProvider, ?>, MeteoriteProvider>definitionBuilder(new ResourceLocation(AppEng.MODID, "meteorite"), ih(new MeteoriteProvider(23))).build();
 	}
 
 	private DefinitionFactory.InputHandler<SkyobjectProvider, SkyobjectProvider> ih(SkyobjectProvider skyobjectGenerator){

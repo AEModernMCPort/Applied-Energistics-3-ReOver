88f24c5c7d3e90256889605bc20f64f029d49cbb
elix-x <Elix-x@users.noreply.github.com>
2017-08-21 22:51:53 +0200

    Temp-fix for provider registration

--- src/main/java/appeng/core/skyfall/skyobject/SkyobjectProviderImpl.java
+++ src/main/java/appeng/core/skyfall/skyobject/SkyobjectProviderImpl.java
@@ -1,6 +1,8 @@
 package appeng.core.skyfall.skyobject;
 
 import appeng.core.skyfall.api.skyobject.SkyobjectProvider;
+import code.elix_x.excomms.reflection.ReflectionHelper;
+import com.google.common.reflect.TypeToken;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
@@ -8,12 +10,17 @@ import java.util.function.Function;
 
 public abstract class SkyobjectProviderImpl<S extends SkyobjectImpl<S, P>, P extends SkyobjectProviderImpl<S, P>> extends IForgeRegistryEntry.Impl<P> implements SkyobjectProvider<S, P> {
 
+	private static final ReflectionHelper.AField<Impl, TypeToken> TOKEN = new ReflectionHelper.AClass<>(IForgeRegistryEntry.Impl.class).<TypeToken>getDeclaredField("token").setAccessible(true);
+	private static final TypeToken<SkyobjectProvider> SKYOBJECTPROVIDERTOKEN = TypeToken.of(SkyobjectProvider.class);
+
 	protected Function<P, S> skyobjectSupplier;
 	protected int defaultWeight;
 
 	public SkyobjectProviderImpl(Function<P, S> skyobjectSupplier, int defaultWeight){
 		this.skyobjectSupplier = skyobjectSupplier;
 		this.defaultWeight = defaultWeight;
+
+		TOKEN.set(this, SKYOBJECTPROVIDERTOKEN);
 	}
 
 	@Override

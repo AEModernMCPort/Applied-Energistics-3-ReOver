8b8c181a5420346ddd7ecd8ff0d36683644c2a00
elix-x <Elix-x@users.noreply.github.com>
2017-08-23 12:22:38 +0200

    Applying forces gathered by event

--- src/main/java/appeng/core/skyfall/skyobject/objects/SkyobjectFallingPhysics.java
+++ src/main/java/appeng/core/skyfall/skyobject/objects/SkyobjectFallingPhysics.java
@@ -6,6 +6,12 @@ import appeng.core.skyfall.api.skyobject.SkyobjectPhysics;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import org.apache.commons.lang3.mutable.MutableObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class SkyobjectFallingPhysics implements SkyobjectPhysics {
 
@@ -15,6 +21,8 @@ public class SkyobjectFallingPhysics implements SkyobjectPhysics {
 
 	protected Vec3d pos = new Vec3d(0, 0, 0);
 	protected Vec3d rot = new Vec3d(0, 0, 0);
+
+	protected double mass;
 
 	public SkyobjectFallingPhysics(Skyobject skyobject){
 		this.skyobject = skyobject;
@@ -56,11 +64,28 @@ public class SkyobjectFallingPhysics implements SkyobjectPhysics {
 		setDirty(true);
 	}
 
+	public double getMass(){
+		return mass;
+	}
+
+	public void setMass(double mass){
+		this.mass = mass;
+	}
+
 	@Override
 	public boolean tick(World world){
-//		setPos(new Vec3d(1000, 1000, -1000));
-		setPos(pos.addVector(-100, -23, 0));
+		List<Vec3d> forces = new ArrayList<>();
+		List<Vec3d> torques = new ArrayList<>();
+		MinecraftForge.EVENT_BUS.post(new GatherForcesEvent(skyobject, forces, torques));
+		if(!forces.isEmpty()) setPos(getPos().add(sumVecs(forces).scale(1/getMass())));
+		if(!torques.isEmpty()) setRot(getRotation().add(sumVecs(torques).scale(1/getMass())));
 		return getPos().y < 0;
+	}
+
+	protected Vec3d sumVecs(Collection<Vec3d> vecs){
+		MutableObject<Vec3d> res = new MutableObject<>(Vec3d.ZERO);
+		vecs.forEach(vec -> res.setValue(res.getValue().add(vec)));
+		return res.getValue();
 	}
 
 	@Override

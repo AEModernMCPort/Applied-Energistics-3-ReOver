40bde024a8a1210afb5472a5570847675dadca9b
Elix_x <Elix-x@users.noreply.github.com>
2018-01-14 13:48:45 +0100

    Added normal cable, changed parts naming order

--- src/main/java/appeng/core/me/definitions/MEPartDefinitions.java
+++ src/main/java/appeng/core/me/definitions/MEPartDefinitions.java
@@ -13,10 +13,12 @@ import net.minecraft.util.ResourceLocation;
 
 public class MEPartDefinitions<P extends Part<P, S>, S extends Part.State<P, S>> extends Definitions<P, IPartDefinition<P, S>> implements IMEPartDefinitions<P, S> {
 
-	private final IPartDefinition<P, S> microGlassCableGreen;
+	private final IPartDefinition<P, S> glassCableMicroGreen;
+	private final IPartDefinition<P, S> glassCableNormalGreen;
 
 	public MEPartDefinitions(DefinitionFactory registry){
-		microGlassCableGreen = registry.<P, IPartDefinition<P, S>, IPartBuilder<P, S, ?>, P>definitionBuilder(new ResourceLocation(AppEng.MODID, "micro_glass_cable_green"), ih(new PartDummy(false))).mesh(new ResourceLocation(AppEng.MODID, "micro/cable/glass/green.obj")).createDefaultPlacerItem().build();
+		glassCableMicroGreen = registry.<P, IPartDefinition<P, S>, IPartBuilder<P, S, ?>, P>definitionBuilder(new ResourceLocation(AppEng.MODID, "glass_cable_micro_green"), ih(new PartDummy(false))).mesh(new ResourceLocation(AppEng.MODID, "cable/glass/micro/green.obj")).createDefaultPlacerItem().build();
+		glassCableNormalGreen = registry.<P, IPartDefinition<P, S>, IPartBuilder<P, S, ?>, P>definitionBuilder(new ResourceLocation(AppEng.MODID, "glass_cable_normal_green"), ih(new PartDummy(true))).mesh(new ResourceLocation(AppEng.MODID, "cable/glass/normal/green.obj")).createDefaultPlacerItem().build();
 		registry.<P, IPartDefinition<P, S>, IPartBuilder<P, S, ?>, P>definitionBuilder(new ResourceLocation(AppEng.MODID, "unlocked_block"), ih(new PartDummy(false))).createDefaultPlacerItem().build();
 		registry.<P, IPartDefinition<P, S>, IPartBuilder<P, S, ?>, P>definitionBuilder(new ResourceLocation(AppEng.MODID, "rotation_test"), ih(new PartDummy(true))).createDefaultPlacerItem().build();
 		registry.<P, IPartDefinition<P, S>, IPartBuilder<P, S, ?>, P>definitionBuilder(new ResourceLocation(AppEng.MODID, "tower"), ih(new PartDummy(true))).createDefaultPlacerItem(part -> new SideIsBottomPlacementLogic<>(part.maybe().get())).build();

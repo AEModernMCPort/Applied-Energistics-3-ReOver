64d45c6ae615f055d7e467c28730b02337ad8092
Elix_x <Elix-x@users.noreply.github.com>
2018-03-10 18:22:25 +0100

    No taxes on imports into the void

--- src/main/java/appeng/core/me/parts/part/device/ImportBus.java
+++ src/main/java/appeng/core/me/parts/part/device/ImportBus.java
@@ -1,11 +1,18 @@
 package appeng.core.me.parts.part.device;
 
+import appeng.core.me.AppEngME;
 import appeng.core.me.api.network.DeviceUUID;
 import appeng.core.me.api.network.NetBlock;
 import appeng.core.me.api.network.device.DeviceRegistryEntry;
+import net.minecraft.util.ITickable;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface ImportBus {
 
@@ -30,7 +37,7 @@ public interface ImportBus {
 
 	}
 
-	class Physical extends PartDeviceD2N.Physical<Part, Physical, Network> {
+	class Physical extends PartDeviceD2N.Physical<Part, Physical, Network> implements ITickable {
 
 		public Physical(Part part){
 			super(part);
@@ -39,6 +46,18 @@ public interface ImportBus {
 		@Override
 		protected Network createNewNetworkCounterpart(){
 			return new Network(getReg(), new DeviceUUID(), null);
+		}
+
+		@Override
+		public void update(){
+			importStuff(AppEngME.INSTANCE.getDevicesHelper().getAllWITargetCPs(this, world).map(cp -> cp.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)).filter(Objects::nonNull));
+		}
+
+		protected void importStuff(Stream<IItemHandler> itemHandlers){
+			oih: for(IItemHandler itemHandler : itemHandlers.collect(Collectors.toSet())) for(int i = 0; i < itemHandler.getSlots(); i++) if(!itemHandler.getStackInSlot(i).isEmpty()){
+				itemHandler.extractItem(i, 1, false);
+				break oih;
+			}
 		}
 
 	}

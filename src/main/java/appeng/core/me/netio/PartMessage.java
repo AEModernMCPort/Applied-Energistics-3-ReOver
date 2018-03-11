c92cfd2082e2ac5c062dae4051954b7b026bc913
Elix_x <Elix-x@users.noreply.github.com>
2018-03-11 22:14:40 +0100

    Sending uuids to keep track of them all

--- src/main/java/appeng/core/me/netio/PartMessage.java
+++ src/main/java/appeng/core/me/netio/PartMessage.java
@@ -1,6 +1,7 @@
 package appeng.core.me.netio;
 
 import appeng.core.me.api.parts.PartPositionRotation;
+import appeng.core.me.api.parts.container.PartUUID;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
@@ -12,6 +13,7 @@ import javax.annotation.Nullable;
 
 public class PartMessage implements IMessage {
 
+	public PartUUID uuid;
 	public PartPositionRotation posRot;
 	public ResourceLocation id;
 	public NBTTagCompound data;
@@ -19,7 +21,8 @@ public class PartMessage implements IMessage {
 	public PartMessage(){
 	}
 
-	public PartMessage(@Nonnull PartPositionRotation posRot, @Nullable ResourceLocation id, @Nullable NBTTagCompound data){
+	public PartMessage(@Nonnull PartUUID uuid, @Nonnull PartPositionRotation posRot, @Nullable ResourceLocation id, @Nullable NBTTagCompound data){
+		this.uuid = uuid;
 		this.posRot = posRot;
 		this.id = id;
 		this.data = data;
@@ -28,6 +31,7 @@ public class PartMessage implements IMessage {
 	@Override
 	public void toBytes(ByteBuf buf){
 		NBTTagCompound nbt = new NBTTagCompound();
+		nbt.setTag("uuid", uuid.serializeNBT());
 		nbt.setTag("posrot", posRot.serializeNBT());
 		if(id != null) nbt.setString("id", id.toString());
 		if(data != null) nbt.setTag("data", data);
@@ -37,6 +41,7 @@ public class PartMessage implements IMessage {
 	@Override
 	public void fromBytes(ByteBuf buf){
 		NBTTagCompound nbt = ByteBufUtils.readTag(buf);
+		uuid = PartUUID.createPartUUID(nbt.getCompoundTag("uuid"));
 		posRot = PartPositionRotation.fromNBT(nbt.getCompoundTag("posrot"));
 		if(nbt.hasKey("id")) id = new ResourceLocation(nbt.getString("id"));
 		if(nbt.hasKey("data")) data = nbt.getCompoundTag("data");

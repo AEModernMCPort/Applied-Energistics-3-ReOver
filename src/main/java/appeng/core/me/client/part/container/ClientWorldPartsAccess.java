15bddc854fdf318160e59b62c7963bc3e856c9aa
Elix_x <Elix-x@users.noreply.github.com>
2018-03-11 18:27:57 +0100

    Dedicated client WPA

---
+++ src/main/java/appeng/core/me/client/part/container/ClientWorldPartsAccess.java
@@ -0,0 +1,12 @@
+package appeng.core.me.client.part.container;
+
+import appeng.core.me.parts.container.WorldPartsAccess;
+import net.minecraft.world.World;
+
+public class ClientWorldPartsAccess extends WorldPartsAccess {
+
+	public ClientWorldPartsAccess(World world){
+		super(world);
+	}
+
+}

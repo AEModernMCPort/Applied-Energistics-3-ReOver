2e354064a2ecd4d2c0a7d6d950edd9c9faf2b2c4
Elix_x <Elix-x@users.noreply.github.com>
2018-04-01 17:23:49 +0200

    Revert "Scheduled debug purpoises"
    This reverts commit 21adc3f46fd6c629a8dd1640a25966c2364a43a2.

--- src/main/java/appeng/core/me/parts/part/device/SimplyPanel.java
+++ src/main/java/appeng/core/me/parts/part/device/SimplyPanel.java
@@ -1,38 +1,18 @@
 package appeng.core.me.parts.part.device;
 
-import appeng.core.me.AppEngME;
 import appeng.core.me.api.network.DeviceUUID;
 import appeng.core.me.api.network.NetBlock;
 import appeng.core.me.api.network.device.DeviceRegistryEntry;
-import net.minecraft.util.ITickable;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public interface SimplyPanel {
 
-	class Network extends PartDeviceD2N.Network<Part, Physical, Network> implements ITickable {
-
-		static int nextId = 0;
-		int id;
+	class Network extends PartDeviceD2N.Network<Part, Physical, Network> {
 
 		public Network(@Nonnull DeviceRegistryEntry<Network, Physical> registryEntry, @Nonnull DeviceUUID uuid, @Nullable NetBlock netBlock){
 			super(registryEntry, uuid, netBlock);
-			id = nextId++;
-		}
-
-		long prevTick = -1;
-		long ticks = 0;
-
-		@Override
-		public void update(){
-			ticks++;
-			if(prevTick == -1) prevTick = System.currentTimeMillis();
-			else if(System.currentTimeMillis() - prevTick > 2500){
-				AppEngME.logger.info(String.format("Panel #%s Tick - %s ticks in 2.5secs on %s", id, ticks, Thread.currentThread().getName()));
-				prevTick = System.currentTimeMillis();
-				ticks = 0;
-			}
 		}
 
 	}

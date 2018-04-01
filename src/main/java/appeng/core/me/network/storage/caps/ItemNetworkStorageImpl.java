3af71dd2accc3f8c3c07f5526b45fe28a7455df0
Elix_x <Elix-x@users.noreply.github.com>
2018-04-01 22:40:50 +0200

    Meet its implementation

---
+++ src/main/java/appeng/core/me/network/storage/caps/ItemNetworkStorageImpl.java
@@ -0,0 +1,19 @@
+package appeng.core.me.network.storage.caps;
+
+import appeng.core.me.api.network.storage.caps.ItemNetworkStorage;
+import appeng.core.me.network.storage.atomic.SubtypedAtomicNetworkStorageImpl;
+import net.minecraft.item.Item;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+
+public class ItemNetworkStorageImpl extends SubtypedAtomicNetworkStorageImpl<ItemNetworkStorage.Entry, Item> implements ItemNetworkStorage {
+
+	public ItemNetworkStorageImpl(){
+		super(Entry::getItem, item -> {
+			NBTTagCompound nbt = new NBTTagCompound();
+			nbt.setString("id", item.getRegistryName().toString());
+			return nbt;
+		}, nbt -> Item.REGISTRY.getObject(new ResourceLocation(nbt.getString("id"))), Entry::serializeNBT, Entry::deserializeNBT);
+	}
+
+}

9876e8c177715ac99de6d904361ca945252be7cd
Elix_x <Elix-x@users.noreply.github.com>
2018-04-12 21:46:31 +0200

    Export bus behavioral overdrive

--- src/main/api/appeng/core/me/api/network/device/ExportBus.java
+++ src/main/api/appeng/core/me/api/network/device/ExportBus.java
@@ -2,10 +2,37 @@ package appeng.core.me.api.network.device;
 
 import appeng.core.me.api.network.NetDevice;
 import appeng.core.me.api.network.PhysicalDevice;
+import appeng.core.me.api.network.device.behavior.BehaviorDriven;
+import appeng.core.me.api.parts.container.PartsAccess;
+import code.elix_x.excomms.optional.NullableOptional;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import java.util.Optional;
 
 public interface ExportBus {
 
-	interface Network<P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> extends NetDevice<N, S> {
+	interface Network<P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> extends NetDevice<N, S>, BehaviorDriven<Behavior> {
+
+		long ticksSinceLastUpdate();
+
+		//TODO Generalize
+		boolean canAcceptForExport();
+
+		//TODO Generalize
+		boolean addForExport(ItemStack stack);
+
+		//TODO Generalize
+		Optional<ItemStack> nextItemForExport();
+
+		//TODO Generalize
+		boolean confirmExport(ItemStack stack);
+
+		//TODO Generalize
+		boolean export(ItemStack stack);
 
 	}
 
@@ -14,6 +41,55 @@ public interface ExportBus {
 	}
 
 	interface Physical<P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> extends PhysicalDevice<N, S>, Part.State<P, S> {
+
+	}
+
+	interface Behavior {
+
+		@Nonnull
+		default <P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> BehaviorDriven.BehaviorOperationResult update(){
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
+
+		@Nonnull
+		default <P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> BehaviorDriven.BehaviorOperationResult updatePhysical(){
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
+
+		@Nonnull
+		default <P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> BehaviorDriven.BehaviorOperationResult onRightClick(@Nonnull PartsAccess.Mutable world, @Nonnull World theWorld, @Nonnull EntityPlayer player, @Nonnull EnumHand hand){
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
+
+		//TODO Generalize
+		@Nonnull
+		default <P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> BehaviorDriven.BehaviorOperationResult canAcceptForExport(){
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
+
+		//TODO Generalize
+		@Nonnull
+		default <P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> BehaviorDriven.BehaviorOperationResult addForExport(ItemStack stack){
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
+
+		//TODO Generalize
+		@Nonnull
+		default <P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> NullableOptional<ItemStack> nextItemForExport(){
+			return NullableOptional.empty();
+		}
+
+		//TODO Generalize
+		@Nonnull
+		default <P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> BehaviorDriven.BehaviorOperationResult confirmExport(ItemStack stack){
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
+
+		//TODO Generalize
+		@Nonnull
+		default <P extends Part<P, S, N>, S extends Physical<P, S, N>, N extends Network<P, S, N>> BehaviorDriven.BehaviorOperationResult exportItem(@Nonnull ItemStack stack){
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
 
 	}
 
--- src/main/java/appeng/core/me/parts/part/device/ExportBusImpl.java
+++ src/main/java/appeng/core/me/parts/part/device/ExportBusImpl.java
@@ -1,14 +1,20 @@
 package appeng.core.me.parts.part.device;
 
+import appeng.core.AppEng;
+import appeng.core.lib.capability.DelegateCapabilityStorage;
+import appeng.core.lib.capability.SingleCapabilityProvider;
 import appeng.core.me.AppEngME;
 import appeng.core.me.api.network.DeviceUUID;
 import appeng.core.me.api.network.NetBlock;
+import appeng.core.me.api.network.NetDevice;
 import appeng.core.me.api.network.device.DeviceRegistryEntry;
 import appeng.core.me.api.network.device.ExportBus;
+import appeng.core.me.api.network.device.behavior.BehaviorDriven;
 import appeng.core.me.api.network.storage.caps.ItemNetworkStorage;
 import appeng.core.me.api.parts.container.PartsAccess;
 import appeng.core.me.network.GlobalNBDManagerImpl;
 import appeng.core.me.network.storage.caps.NetworkStorageCaps;
+import code.elix_x.excomms.optional.NullableOptional;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -16,9 +22,18 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import org.apache.commons.lang3.mutable.MutableInt;
 import org.apache.commons.lang3.mutable.MutableObject;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
@@ -31,50 +46,50 @@ import java.util.stream.Stream;
 
 public interface ExportBusImpl extends ExportBus {
 
-	class Network extends PartDeviceD2N.Network<Part, Physical, Network> implements ExportBus.Network<Part, Physical, Network>, ITickable {
+	class Network extends PartDeviceD2N.BehaviorDriven.Network<Part, Physical, Network, Behavior> implements ExportBus.Network<Part, Physical, Network>, ITickable {
 
 		public Network(@Nonnull DeviceRegistryEntry<Network, Physical> registryEntry, @Nonnull DeviceUUID uuid, @Nullable NetBlock netBlock){
 			super(registryEntry, uuid, netBlock);
 		}
 
-		ItemNetworkStorage.Entry selected;
-		protected Queue<ItemStack> exportBuffer = new LinkedList<>();
-
-		protected int bufferLimit = 1;
-		protected double oneItemPerTicks = 10;
-
 		protected long lastUpdate; //FIXME transient or serialized?
+		protected long sinceLastUpdate;
+
+		@Override
+		public long ticksSinceLastUpdate(){
+			return sinceLastUpdate;
+		}
 
 		@Override
 		public void update(){
 			long current = GlobalNBDManagerImpl.getInstance().currentServerTick();
-			if(current - lastUpdate > oneItemPerTicks){
-				if(selected != null && exportBuffer.size() < bufferLimit){
-					lastUpdate = current;
-					getNetwork().ifPresent(network -> {
-						int ext = network.getCapability(NetworkStorageCaps.item, null).store(selected, -(int) Math.ceil(1 / oneItemPerTicks), -1);
-						if(ext < 0) exportBuffer.add(selected.asStack(-ext));
-					});
-				}
-			}
+			sinceLastUpdate = current - lastUpdate;
+			if(behaviorDrivenAction(Behavior::update).filter(res -> res == BehaviorOperationResult.SUCCESS).isPresent()) lastUpdate = current;
 		}
 
 		@Override
-		public NBTTagCompound serializeNBT(){
-			NBTTagCompound nbt = super.serializeNBT();
-			if(selected != null) nbt.setTag("selected", selected.serializeNBT());
-			NBTTagList queue = new NBTTagList();
-			exportBuffer.stream().map(ItemStack::serializeNBT).forEach(queue::appendTag);
-			nbt.setTag("buffer", queue);
-			return nbt;
+		public boolean canAcceptForExport(){
+			return behaviorDrivenAction(Behavior::canAcceptForExport).orElse(BehaviorOperationResult.FAIL) == BehaviorOperationResult.SUCCESS;
 		}
 
 		@Override
-		protected void deserializeNBT(NBTTagCompound nbt){
-			super.deserializeNBT(nbt);
-			if(nbt.hasKey("selected")) selected = ItemNetworkStorage.Entry.deserializeNBT(nbt.getCompoundTag("selected"));
-			exportBuffer.clear();
-			((Iterable<NBTTagCompound>) nbt.getTag("buffer")).forEach(tag -> exportBuffer.add(new ItemStack(tag)));
+		public boolean addForExport(ItemStack stack){
+			return behaviorDrivenAction(behavior -> behavior.addForExport(stack)).orElse(BehaviorOperationResult.FAIL) == BehaviorOperationResult.SUCCESS;
+		}
+
+		@Override
+		public Optional<ItemStack> nextItemForExport(){
+			return behaviorDrivenGetAction(Behavior::nextItemForExport).orElseOpt(Optional.empty());
+		}
+
+		@Override
+		public boolean confirmExport(ItemStack stack){
+			return behaviorDrivenAction(behavior -> behavior.confirmExport(stack)).orElse(BehaviorOperationResult.FAIL) == BehaviorOperationResult.SUCCESS;
+		}
+
+		@Override
+		public boolean export(ItemStack stack){
+			return behaviorDrivenAction(behavior -> behavior.exportItem(stack)).orElse(BehaviorOperationResult.FAIL) == BehaviorOperationResult.SUCCESS;
 		}
 
 	}
@@ -92,13 +107,13 @@ public interface ExportBusImpl extends ExportBus {
 
 		@Override
 		public EnumActionResult onRightClick(@Nullable Physical part, @Nonnull PartsAccess.Mutable world, @Nonnull World theWorld, @Nonnull EntityPlayer player, @Nonnull EnumHand hand){
-			if(!theWorld.isRemote) part.networkCounterpart.selected = ItemNetworkStorage.Entry.ofItemStack(player.getHeldItem(hand));
+			if(!theWorld.isRemote) part.networkCounterpart.behaviorDrivenAction(behavior -> behavior.onRightClick(world, theWorld, player, hand));
 			return EnumActionResult.SUCCESS;
 		}
 
 	}
 
-	class Physical extends PartDeviceD2N.Physical<Part, Physical, Network> implements ExportBus.Physical<Part, Physical, Network>, ITickable {
+	class Physical extends PartDeviceD2N.BehaviorDriven.Physical<Part, Physical, Network, Behavior> implements ExportBus.Physical<Part, Physical, Network>, ITickable {
 
 		public Physical(Part part){
 			super(part);
@@ -111,28 +126,153 @@ public interface ExportBusImpl extends ExportBus {
 
 		@Override
 		public void update(){
-			exportStuff(AppEngME.INSTANCE.getDevicesHelper().getAllWITargetCPs(this, world).map(cp -> cp.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)).filter(Objects::nonNull));
+			networkCounterpart.behaviorDrivenAction(Behavior::updatePhysical);
 		}
 
-		protected void exportStuff(Stream<IItemHandler> itemHandlers){
-			if(networkCounterpart.hasNetwork()){
-				ItemStack next = networkCounterpart.exportBuffer.peek();
-				if(next != null){
-					List<Pair<IItemHandler, Integer>> dests = new ArrayList<>();
-					oeh: for(IItemHandler itemHandler : itemHandlers.collect(Collectors.toSet())) for(int i = 0; i < itemHandler.getSlots(); i++){
-						ItemStack rem = itemHandler.insertItem(i, next, true);
-						if(rem.getCount() < next.getCount()){
-							next = rem;
-							dests.add(new ImmutablePair<>(itemHandler, i));
-							if(next.isEmpty()) break oeh;
-						}
-					}
-					if(next.isEmpty()){
-						MutableObject<ItemStack> rem = new MutableObject<>(networkCounterpart.exportBuffer.poll());
-						dests.forEach(ihSlot -> rem.setValue(ihSlot.getLeft().insertItem(ihSlot.getRight(), rem.getValue(), false)));
-					}
+	}
+
+	@Mod.EventBusSubscriber(modid = AppEng.MODID)
+	class DefaultBehavior implements Behavior {
+
+		@CapabilityInject(Data.class)
+		public static Capability<Data> dataCap;
+
+		public static void registerCaps(){
+			CapabilityManager.INSTANCE.register(Data.class, new DelegateCapabilityStorage<>(), Data::new);
+		}
+
+		@SubscribeEvent
+		public static void addDefaultData(AttachCapabilitiesEvent<NetDevice> event){
+			if(event.getObject() instanceof ExportBus.Network) event.addCapability(new ResourceLocation(AppEng.MODID, "export_bus_default_data"), new SingleCapabilityProvider.Serializeable<>(dataCap, new Data()));
+		}
+
+		@SubscribeEvent
+		public static <N extends ExportBus.Network<P, S, N>, S extends ExportBus.Physical<P, S, N>, P extends ExportBus.Part<P, S, N>> void addDefaultBehavior(BehaviorDriven.AttachDeviceBehaviorEvent<ExportBus.Network> eve){
+			eve.<N, S, ExportBus.Behavior>event(ExportBus.Network.class).ifPresent(event -> event.addBehavior(new DefaultBehavior((Network) event.getDevice()), 0));
+		}
+
+		public static class Data implements INBTSerializable<NBTTagCompound> {
+
+			ItemNetworkStorage.Entry selected;
+			int bufferLimit = 1;
+			double oneItemPerTicks = 10;
+
+			Queue<ItemStack> buffer = new LinkedList<>();
+
+			@Override
+			public NBTTagCompound serializeNBT(){
+				NBTTagCompound nbt = new NBTTagCompound();
+				if(selected != null) nbt.setTag("selected", selected.serializeNBT());
+				NBTTagList queue = new NBTTagList();
+				buffer.stream().map(ItemStack::serializeNBT).forEach(queue::appendTag);
+				nbt.setTag("buffer", queue);
+				return nbt;
+			}
+
+			@Override
+			public void deserializeNBT(NBTTagCompound nbt){
+				if(nbt.hasKey("selected")) selected = ItemNetworkStorage.Entry.deserializeNBT(nbt.getCompoundTag("selected"));
+				buffer.clear();
+				((Iterable<NBTTagCompound>) nbt.getTag("buffer")).forEach(tag -> buffer.add(new ItemStack(tag)));
+			}
+
+		}
+
+		protected final Network exportBus;
+		protected final Data data;
+
+		public DefaultBehavior(Network exportBus){
+			this.exportBus = exportBus;
+			data = this.exportBus.getCapability(dataCap, null);
+		}
+
+		@Nonnull
+		@Override
+		public <P extends ExportBus.Part<P, S, N>, S extends ExportBus.Physical<P, S, N>, N extends ExportBus.Network<P, S, N>> BehaviorDriven.BehaviorOperationResult update(){
+			if(exportBus.ticksSinceLastUpdate() > data.oneItemPerTicks){
+				if(data.selected != null && exportBus.canAcceptForExport()){
+					MutableInt res = new MutableInt();
+					exportBus.getNetwork().ifPresent(network -> {
+						int ext = network.getCapability(NetworkStorageCaps.item, null).store(data.selected, -(int) Math.ceil(1 / data.oneItemPerTicks), -1);
+						if(ext < 0) if(!exportBus.addForExport(data.selected.asStack(-ext))) throw new IllegalArgumentException("How? Seriously. I asked whether you could accept, and you said \"YES!\", but when i did it, you failed me... WHYYYYYY D; ???");
+						res.setValue(ext);
+					});
+					return res.intValue() < 0 ? BehaviorDriven.BehaviorOperationResult.SUCCESS : BehaviorDriven.BehaviorOperationResult.FAIL;
 				}
 			}
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
+
+		@Nonnull
+		@Override
+		public <P extends ExportBus.Part<P, S, N>, S extends ExportBus.Physical<P, S, N>, N extends ExportBus.Network<P, S, N>> BehaviorDriven.BehaviorOperationResult updatePhysical(){
+			return exportBus.nextItemForExport().map(exportBus::export).map(b -> b ? BehaviorDriven.BehaviorOperationResult.SUCCESS : BehaviorDriven.BehaviorOperationResult.FAIL).orElse(BehaviorDriven.BehaviorOperationResult.PASS);
+		}
+
+		@Nonnull
+		@Override
+		public <P extends ExportBus.Part<P, S, N>, S extends ExportBus.Physical<P, S, N>, N extends ExportBus.Network<P, S, N>> BehaviorDriven.BehaviorOperationResult onRightClick(@Nonnull PartsAccess.Mutable world, @Nonnull World theWorld, @Nonnull EntityPlayer player, @Nonnull EnumHand hand){
+			data.selected = ItemNetworkStorage.Entry.ofItemStack(player.getHeldItem(hand));
+			return BehaviorDriven.BehaviorOperationResult.SUCCESS;
+		}
+
+		@Nonnull
+		@Override
+		public <P extends ExportBus.Part<P, S, N>, S extends ExportBus.Physical<P, S, N>, N extends ExportBus.Network<P, S, N>> BehaviorDriven.BehaviorOperationResult canAcceptForExport(){
+			return data.buffer.size() < data.bufferLimit ? BehaviorDriven.BehaviorOperationResult.SUCCESS : BehaviorDriven.BehaviorOperationResult.FAIL;
+		}
+
+		@Nonnull
+		@Override
+		public <P extends ExportBus.Part<P, S, N>, S extends ExportBus.Physical<P, S, N>, N extends ExportBus.Network<P, S, N>> BehaviorDriven.BehaviorOperationResult addForExport(ItemStack stack){
+			if(data.buffer.size() < data.bufferLimit){
+				data.buffer.add(stack);
+				return BehaviorDriven.BehaviorOperationResult.SUCCESS;
+			}
+			return BehaviorDriven.BehaviorOperationResult.FAIL;
+		}
+
+		@Nonnull
+		@Override
+		public <P extends ExportBus.Part<P, S, N>, S extends ExportBus.Physical<P, S, N>, N extends ExportBus.Network<P, S, N>> NullableOptional<ItemStack> nextItemForExport(){
+			return NullableOptional.of(data.buffer.peek());
+		}
+
+		@Nonnull
+		@Override
+		public <P extends ExportBus.Part<P, S, N>, S extends ExportBus.Physical<P, S, N>, N extends ExportBus.Network<P, S, N>> BehaviorDriven.BehaviorOperationResult confirmExport(ItemStack stack){
+			if(data.buffer.peek() == stack){
+				data.buffer.poll();
+				return BehaviorDriven.BehaviorOperationResult.SUCCESS;
+			}
+			return BehaviorDriven.BehaviorOperationResult.PASS;
+		}
+
+		@Nonnull
+		@Override
+		public <P extends ExportBus.Part<P, S, N>, S extends ExportBus.Physical<P, S, N>, N extends ExportBus.Network<P, S, N>> BehaviorDriven.BehaviorOperationResult exportItem(@Nonnull ItemStack stack){
+			if(!exportBus.getPhysicalCounterpart().isPresent()) return BehaviorDriven.BehaviorOperationResult.PASS;
+			Physical exportBusPhysical = exportBus.getPhysicalCounterpart().get();
+			return exportStuff(stack, AppEngME.INSTANCE.getDevicesHelper().getAllWITargetCPs(exportBusPhysical, exportBusPhysical.world).map(cp -> cp.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)).filter(Objects::nonNull));
+		}
+
+		protected BehaviorDriven.BehaviorOperationResult exportStuff(ItemStack exp, Stream<IItemHandler> itemHandlers){
+			ItemStack next = exp.copy();
+			List<Pair<IItemHandler, Integer>> dests = new ArrayList<>();
+			oeh: for(IItemHandler itemHandler : itemHandlers.collect(Collectors.toSet())) for(int i = 0; i < itemHandler.getSlots(); i++){
+				ItemStack rem = itemHandler.insertItem(i, next, true);
+				if(rem.getCount() < next.getCount()){
+					next = rem;
+					dests.add(new ImmutablePair<>(itemHandler, i));
+					if(next.isEmpty()) break oeh;
+				}
+			}
+			if(next.isEmpty() && exportBus.confirmExport(exp)){
+				MutableObject<ItemStack> rem = new MutableObject<>(exp);
+				dests.forEach(ihSlot -> rem.setValue(ihSlot.getLeft().insertItem(ihSlot.getRight(), rem.getValue(), false)));
+				return BehaviorDriven.BehaviorOperationResult.SUCCESS;
+			}
+			return BehaviorDriven.BehaviorOperationResult.FAIL;
 		}
 
 	}

e7dd5b44785b531062431c98564573679cef5cef
Elix_x <Elix-x@users.noreply.github.com>
2018-04-13 22:28:10 +0200

    🔥 CrSpWo [API] :fireworks:

--- src/main/api/appeng/core/crafting/api/ICrafting.java
+++
@@ -1,9 +0,0 @@
-package appeng.core.crafting.api;
-
-import appeng.api.definitions.IDefinitionsProvider;
-
-public interface ICrafting extends IDefinitionsProvider {
-
-	String NAME = "crafting";
-
-}
--- src/main/api/appeng/core/crafting/api/definitions/ICraftingItemDefinitions.java
+++
@@ -1,9 +0,0 @@
-package appeng.core.crafting.api.definitions;
-
-import appeng.api.definitions.IDefinitions;
-import appeng.core.core.api.definition.IItemDefinition;
-import net.minecraft.item.Item;
-
-public interface ICraftingItemDefinitions extends IDefinitions<Item, IItemDefinition<Item>> {
-
-}
